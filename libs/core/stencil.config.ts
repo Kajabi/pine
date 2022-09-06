63332fc7a02f00f92b8cb1c1334210cbbcc0dbec
Julian Skinner <ju.skinner@gmail.com>
2022-09-06 14:52:43 -0500

    chore(setup): nxext add react output target
    This was generated by running @nxext/stencil:add-outputtarget core

--- libs/core/stencil.config.ts
+++ libs/core/stencil.config.ts
@@ -1,11 +1,13 @@
 import { Config } from '@stencil/core';
-
+import { reactOutputTarget } from '@stencil/react-output-target';
 import { sass } from '@stencil/sass';
 
 export const config: Config = {
+  buildEs5: 'prod',
+  extras: {},
   namespace: 'sage-core',
-  taskQueue: 'async',
   plugins: [sass()],
+  taskQueue: 'async',
   outputTargets: [
     {
       type: 'dist',
@@ -18,6 +20,19 @@ export const config: Config = {
     },
     {
       type: 'docs-readme',
+      footer: ' ',
     },
+    {
+      type: 'docs-vscode',
+      file: 'vscode-data.json',
+    },
+    reactOutputTarget({
+      componentCorePackage: '@sage/core',
+      includeDefineCustomElements: true,
+      proxiesFile: '../../dist/core-react/proxies.ts',
+    }),
   ],
+  testing: {
+    allowableMismatchedPixels: 200,
+  },
 };
