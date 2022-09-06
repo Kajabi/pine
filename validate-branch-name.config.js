895f5ad2d6800d6d655b5ee0e884d14f2baa94b5
Julian Skinner <ju.skinner@gmail.com>
2022-09-06 16:04:22 -0500

    chore(setup): add validate-branch-name
    Validates branch name against pattern in validate-branch-name.config.js

---
+++ validate-branch-name.config.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  pattern: '^(develop|main|release)$|^(chore|ci|docs|feature|fix|hotfix|perf|refactor|revert|style|test)\/([a-zA-Z-].+)$',
+  errorMsg: 'Invalid branch name'
+}
