9abd180ec469fde61b3f7404010a68af4327d1b0
Ashley E <ashleyne92@gmail.com>
2022-10-10 16:18:34 -0500

    style(link): adds appropriate class to link component and uses it for styling

--- libs/core/src/components/sage-link/sage-link.tsx
+++ libs/core/src/components/sage-link/sage-link.tsx
@@ -49,7 +49,7 @@ export class SageLink {
     );
 
     return (
-      <a href={this.href} class={`sage-link--${this.variant} sage-link--${this.fontSize} `} target={this.external ? '_blank' : undefined}>
+      <a href={this.href} class={`sage-link sage-link--${this.variant} sage-link--${this.fontSize} `} target={this.external ? '_blank' : undefined}>
         <slot>{this.href}</slot>
         {this.external == true && externalIcon}
       </a>
