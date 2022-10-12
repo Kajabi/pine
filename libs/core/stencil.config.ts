d69bf4ecd6c6eb3b3807ea79685e475543485000
Julian Skinner <ju.skinner@gmail.com>
2022-10-11 22:28:27 -0500

    chore(setup): add stenciljs project

---
+++ libs/core/stencil.config.ts
@@ -0,0 +1,37 @@
+import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
+
+// Plugins
+import { sass } from '@stencil/sass';
+
+export const config: Config = {
+  namespace: 'sage-core',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+      autoDefineCustomElements: true,
+      generateTypeDeclarations: true,
+    },
+    {
+      type: 'docs-readme',
+      footer: '',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+    reactOutputTarget({
+      componentCorePackage: '@sage/core',
+      includeDefineCustomElements: true,
+      includePolyfills: true,
+      proxiesFile: '../../libs/react/src/components/proxies.ts',
+    }),
+  ],
+  buildEs5: 'prod',
+  plugins: [sass()],
+  taskQueue: 'async',
+};
---
+++ libs/react/src/components/proxies.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@sage/core';
+
+import { applyPolyfills, defineCustomElements } from '@sage/core/loader';
+
+applyPolyfills().then(() => defineCustomElements());
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
