fce6be9a62ab4a0aef3127eae8179cded74cd01c
Ashley E <ashleyne92@gmail.com>
2022-10-27 16:39:16 -0500

    chore: formatting

--- libs/core/src/components/sage-image/sage-image.tsx
+++ libs/core/src/components/sage-image/sage-image.tsx
@@ -9,45 +9,45 @@ export class SageImage {
   /**
    * The image's alt tag. If none is provided,
    * it will default to an empty string.
-  */
- @Prop() alt? = '';
+   */
+  @Prop() alt? = '';
 
- /**
-  * The height of the image in pixels. Setting this will
-  * devote space in the layout to prevent layout
-  * shifts when the image is loaded.
-  */
- @Prop() height?: number;
+  /**
+   * The height of the image in pixels. Setting this will
+   * devote space in the layout to prevent layout
+   * shifts when the image is loaded.
+   */
+  @Prop() height?: number;
 
- /**
-  * Indicates how the browser should load the image.
-  * Defaults to "eager".
-  */
- @Prop() loading?: 'eager' | 'lazy' = 'eager';
+  /**
+   * Indicates how the browser should load the image.
+   * Defaults to "eager".
+   */
+  @Prop() loading?: 'eager' | 'lazy' = 'eager';
 
- /**
-  * Determines the intended display size of an image
-  * within certian breakpoints. Has no effect if `srcset`
-  * is not set or value has no width descriptor.
-  */
- @Prop() sizes?: string;
+  /**
+   * Determines the intended display size of an image
+   * within certian breakpoints. Has no effect if `srcset`
+   * is not set or value has no width descriptor.
+   */
+  @Prop() sizes?: string;
 
- /**
-  * The image's source.
-  */
- @Prop() src: string;
+  /**
+   * The image's source.
+   */
+  @Prop() src: string;
 
- /**
-  * A set of image sources for the browser to use.
-  */
- @Prop() srcset?: string
+  /**
+   * A set of image sources for the browser to use.
+   */
+  @Prop() srcset?: string
 
- /**
-  * The width of the image in pixels. Setting this will
-  * devote space in the layout to prevent layout
-  * shifts when the image is loaded.
-  */
- @Prop() width?: number;
+  /**
+   * The width of the image in pixels. Setting this will
+   * devote space in the layout to prevent layout
+   * shifts when the image is loaded.
+   */
+  @Prop() width?: number;
 
   render() {
     return (
