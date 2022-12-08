4f609775db1b9ae96ae2a8be1ec2fd0475224963
Christopher Goodwin <christopher.goodwin@kajabi.com>
2022-12-08 11:15:26 -0500

    fix(button css): update custom properties structure/naming

--- libs/core/src/components/sage-button/sage-button.tsx
+++ libs/core/src/components/sage-button/sage-button.tsx
@@ -22,7 +22,7 @@ export class SageButton {
    * @defaultValue false
    */
   @Prop() disabled? = false;
-
+  
   render() {
     const trashIcon = (
       <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
