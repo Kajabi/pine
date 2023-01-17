863b4bbf85afde45fbbcf7d26e4e9dc3fcb945da
Julian Skinner <ju.skinner@gmail.com>
2023-01-17 16:54:48 -0600

    feat(sage-react): add the 'sage-icon' library to the project

--- libs/react/src/components/proxies.ts
+++ libs/react/src/components/proxies.ts
@@ -10,6 +10,7 @@ import { applyPolyfills, defineCustomElements } from '@sage/core/loader';
 applyPolyfills().then(() => defineCustomElements());
 export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
 export const SageButton = /*@__PURE__*/createReactComponent<JSX.SageButton, HTMLSageButtonElement>('sage-button');
+export const SageIcon = /*@__PURE__*/createReactComponent<JSX.SageIcon, HTMLSageIconElement>('sage-icon');
 export const SageImage = /*@__PURE__*/createReactComponent<JSX.SageImage, HTMLSageImageElement>('sage-image');
 export const SageInput = /*@__PURE__*/createReactComponent<JSX.SageInput, HTMLSageInputElement>('sage-input');
 export const SageLink = /*@__PURE__*/createReactComponent<JSX.SageLink, HTMLSageLinkElement>('sage-link');
