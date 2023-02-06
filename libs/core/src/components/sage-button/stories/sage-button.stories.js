05b9c09fd41f494c2b4873646356d5206d22c064
Quinton Jason <quinton.jason@gmail.com>
2023-02-06 13:20:38 -0600

    chore(input): trying to add @sage/core to the monorepo

--- libs/core/src/components/sage-button/stories/sage-button.stories.js
+++ libs/core/src/components/sage-button/stories/sage-button.stories.js
@@ -1,7 +1,21 @@
 import { html, render } from 'lit-html';
+import { extractArgTypes } from '@pxtrn/storybook-addon-docs-stencil';
 
 const BaseTemplate = (args) => html`<sage-button  disabled=${args.disabled} icon=${args.icon} name=${args.name} type=${args.type} value=${args.value} variant=${args.variant}>${args.slot}</sage-button> `;
 const defaultParameters = { docs: { disable: true } };
+
+import MDX from './sage-button.docs.mdx';
+
+export default {
+  title: "webcomponents/Button",
+  component: 'sage-button',
+  argTypes: extractArgTypes('sage-button'),
+  parameters: {
+    docs: {
+      page: MDX,
+    }
+  }
+}
 
 export const Accent = BaseTemplate.bind();
 Accent.args = {
