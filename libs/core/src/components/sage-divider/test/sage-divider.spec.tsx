06ed24c8607e2d27da58df0cc30605531fb24327
Phillip Lovelace <phillip@pixelflips.com>
2023-04-04 14:52:19 -0700

    test(divider): add additional tests and increase coverage

--- libs/core/src/components/sage-divider/test/sage-divider.spec.tsx
+++ libs/core/src/components/sage-divider/test/sage-divider.spec.tsx
@@ -5,14 +5,40 @@ describe('sage-divider', () => {
   it('renders', async () => {
     const page = await newSpecPage({
       components: [SageDivider],
-      html: `<sage-divider></sage-divider>`,
+      html: `<sage-divider />`,
     });
     expect(page.root).toEqualHtml(`
       <sage-divider>
         <mock:shadow-root>
-          <slot></slot>
+          <hr class="sage-divider">
         </mock:shadow-root>
       </sage-divider>
     `);
   });
+  it('renders vertically when vertical prop is set', async () => {
+    const page = await newSpecPage({
+      components: [SageDivider],
+      html: `<sage-divider vertical="true" />`,
+    });
+    expect(page.root).toEqualHtml(`
+      <sage-divider vertical="true">
+        <mock:shadow-root>
+          <hr class="sage-divider sage-divider--vertical">
+        </mock:shadow-root>
+      </sage-divider>
+    `)
+  })
+  it('renders with offset applied when prop is set', async () => {
+    const page = await newSpecPage({
+      components: [SageDivider],
+      html: `<sage-divider offset="lg" />`,
+    });
+    expect(page.root).toEqualHtml(`
+      <sage-divider offset="lg">
+        <mock:shadow-root>
+          <hr class="sage-divider sage-divider--offset-lg">
+        </mock:shadow-root>
+      </sage-divider>
+    `)
+  })
 });
