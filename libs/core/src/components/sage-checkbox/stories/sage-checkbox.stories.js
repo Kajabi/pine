dfdfe63dc33aa268c01be5efe6b9346b0ac8cf21
Phillip Lovelace <phillip@pixelflips.com>
2023-04-06 15:33:49 -0700

    docs(checkbox): add initial stories

---
+++ libs/core/src/components/sage-checkbox/stories/sage-checkbox.stories.js
@@ -0,0 +1,56 @@
+import { html } from 'lit-html';
+
+const BaseTemplate = (args) =>
+  html` <sage-checkbox
+    id=${args.id}
+    label=${args.label}
+    checked=${args.checked}
+    disabled=${args.disabled}
+    message=${args.message}
+    name=${args.name}
+    indeterminate=${args.indeterminate}
+    required=${args.required}
+    value=${args.value}
+  />`;
+
+const defaultParameters = { docs: { disable: true } };
+
+export const Default = BaseTemplate.bind();
+Default.args = {
+  id: 'default',
+  label: 'Label text',
+};
+Default.parameters = { ...defaultParameters };
+
+export const Checked = BaseTemplate.bind();
+Checked.args = {
+  id: 'checked',
+  label: 'Label text',
+  checked: true,
+};
+Checked.parameters = { ...defaultParameters };
+
+export const Disabled = BaseTemplate.bind();
+Disabled.args = {
+  id: 'disabled',
+  label: 'Label text',
+  disabled: true,
+};
+Disabled.parameters = { ...defaultParameters };
+
+export const Indeterminate = BaseTemplate.bind();
+Indeterminate.args = {
+  id: 'indeterminate',
+  label: 'Label text',
+  indeterminate: true,
+};
+Indeterminate.parameters = { ...defaultParameters };
+
+
+export const withMessage = BaseTemplate.bind();
+withMessage.args = {
+  id: 'indeterminite',
+  label: 'Label text',
+  message: "This is short message text",
+};
+withMessage.parameters = { ...defaultParameters };
