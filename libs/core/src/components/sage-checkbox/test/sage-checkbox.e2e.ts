c6250adfeff84b27cd640186f8e9feb66bc605aa
Phillip Lovelace <phillip@pixelflips.com>
2023-04-17 10:31:18 -0700

    test(checkbox): refactor e2e test based on convos and review

--- libs/core/src/components/sage-checkbox/test/sage-checkbox.e2e.ts
+++ libs/core/src/components/sage-checkbox/test/sage-checkbox.e2e.ts
@@ -1,148 +1,44 @@
 import { newE2EPage } from '@stencil/core/testing';
 
 describe('sage-checkbox', () => {
-  it('renders', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<sage-checkbox />');
-
-    const element = await page.find('sage-checkbox');
-    expect(element).toHaveClass('hydrated');
-  });
-
-  it('renders with id when checkboxId prop is set', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<sage-checkbox checkbox-id="default" />');
-
-    const element = await page.find('sage-checkbox');
-    expect(element).toHaveClass('hydrated');
-
-    const checkbox = await page.find('sage-checkbox >>> input');
-    expect(checkbox?.id).toEqual('default');
-  });
-
-  it('renders checkbox as checked', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<sage-checkbox checkbox-id="default" checked />');
-
-    const element = await page.find('sage-checkbox');
-    expect(element).toHaveClass('hydrated');
-
-    const checkbox = await page.find('sage-checkbox >>> input#default');
-    const checked = await checkbox.getProperty('checked');
-    expect(await checked).toEqual(true);
-  });
-
-  it('renders checkbox as unchecked', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<sage-checkbox checkbox-id="default" />');
-
-    const element = await page.find('sage-checkbox');
-    expect(element).toHaveClass('hydrated');
-
-    const checkbox = await page.find('sage-checkbox >>> input#default');
-    const checked = await checkbox.getProperty('checked');
-    expect(await checked).toEqual(false);
-  });
-
-  it('renders checkbox in disabled state', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<sage-checkbox checkbox-id="default" disabled />');
-
-    const element = await page.find('sage-checkbox');
-    expect(element).toHaveClass('hydrated');
-
-    const checkbox = await page.find('sage-checkbox >>> input#default');
-    const disabled = await checkbox.getProperty('disabled');
-    expect(await disabled).toEqual(true);
-  });
-
-  it('renders checkbox in invalid state', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<sage-checkbox checkbox-id="default" invalid />');
-
-    const element = await page.find('sage-checkbox');
-    expect(element).toHaveClass('hydrated');
-
-    const checkbox = await page.find('sage-checkbox >>> div');
-    expect(await checkbox).toHaveClass('is-invalid');
-  });
-
-  it('renders checkbox in indeterminate state', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<sage-checkbox checkbox-id="default" indeterminate />');
-
-    const element = await page.find('sage-checkbox');
-    expect(await element).toHaveClass('hydrated');
-
-    const checkbox = await page.find('sage-checkbox >>> div');
-    expect(await checkbox).toHaveClass('is-indeterminate');
-  });
-
-  it('renders label text', async () => {
+  it('toggles checked and unchecked', async () => {
     const page = await newE2EPage();
     await page.setContent('<sage-checkbox checkbox-id="default" label="Label text" />');
-
-    const element = await page.find('sage-checkbox');
-    expect(element).toHaveClass('hydrated');
-
-    const label = await page.find('sage-checkbox >>> label');
-    expect(await label.innerText).toBe('Label text');
+    const component = await page.find('sage-checkbox');
+    expect(component).toHaveClass('hydrated');
+    const checkbox = await page.find('sage-checkbox >>> input');
+    await checkbox.click();
+    expect(await checkbox.getProperty('checked')).toBeTruthy();
+    await checkbox.click();
+    expect(await checkbox.getProperty('checked')).toBeFalsy();
   });
-
-  it('renders message text', async () => {
+  it('toggles input disabled state', async () => {
     const page = await newE2EPage();
-    await page.setContent('<sage-checkbox checkbox-id="default" message="This is short message text" />');
-
-    const element = await page.find('sage-checkbox');
-    expect(element).toHaveClass('hydrated');
-
-    const message = await page.find('sage-checkbox >>> .sage-checkbox__message');
-    expect(await message.innerText).toBe('This is short message text');
+    await page.setContent('<sage-checkbox checkbox-id="default" label="Label text" />');
+    const component = await page.find('sage-checkbox');
+    expect(component).toHaveClass('hydrated');
+    let checkbox = await page.find('sage-checkbox >>> input');
+    component.setProperty('disabled', false);
+    await page.waitForChanges();
+    expect(await checkbox.getProperty('disabled')).toBe(false);
+    component.setProperty('disabled', true);
+    await page.waitForChanges();
+    expect(await checkbox.getProperty('disabled')).toBe(true);
   });
-
-  it('renders checkbox as required', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<sage-checkbox checkbox-id="default" required />');
-
-    const element = await page.find('sage-checkbox');
-    expect(element).toHaveClass('hydrated');
-
-    const checkbox = await page.find('sage-checkbox >>> input#default');
-    const required = await checkbox.getProperty('required');
-    expect(await required).toEqual(true);
-  });
-
-  it('sets input value attribute', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<sage-checkbox checkbox-id="default" value="This is the input value" />');
-
-    const element = await page.find('sage-checkbox');
-    expect(element).toHaveClass('hydrated');
-
-    const checkbox = await page.find('sage-checkbox >>> input#default');
-    const value = await checkbox.getProperty('value');
-    expect(await value).toBe('This is the input value');
-  });
-
   it('emits "sageCheckboxChange" event when checkbox is changed', async () => {
     const page = await newE2EPage();
     await page.setContent('<sage-checkbox checkbox-id="default" label="Label text" />');
     const checkbox = await page.find('sage-checkbox >>> input');
-
     const eventSpy = await page.spyOnEvent('sageCheckboxChange');
     await checkbox.press('Space');
-
     expect(eventSpy).toHaveReceivedEvent();
   });
-
   it('does not emit "sageCheckboxChange" event when checkbox is changed and disabled', async () => {
     const page = await newE2EPage();
     await page.setContent('<sage-checkbox checkbox-id="default" label="Label text" disabled />');
     const checkbox = await page.find('sage-checkbox >>> input');
-
     const eventSpy = await page.spyOnEvent('sageCheckboxChange');
     await checkbox.press('Space');
-
     expect(eventSpy).not.toHaveReceivedEvent();
   });
 });
