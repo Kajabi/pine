fef629905212cabd403e9b1be0c2d7d7b758f977
Quinton Jason <quinton.jason@gmail.com>
2023-04-19 08:15:51 -0500

    test(tooltip): update e2e test

--- libs/core/src/components/sage-tooltip/test/sage-tooltip.e2e.ts
+++ libs/core/src/components/sage-tooltip/test/sage-tooltip.e2e.ts
@@ -6,12 +6,15 @@ describe('sage-tooltip', () => {
     await page.setContent('<sage-tooltip content="tooltip content"><button id="trigger">Secondary</button></sage-tooltip>');
 
     const trigger = await page.find('#trigger');
-    const overlay = await page.find('sage-tooltip >>> .sage-tooltip');
+    const component = await page.find('sage-tooltip >>> .sage-tooltip');
+    const overlay = await page.find('sage-tooltip >>> .sage-tooltip__content');
 
     trigger.focus();
     await page.waitForChanges();
 
-    expect(overlay).toHaveClass('sage-tooltip--is-open');
+    expect(component).toHaveClass('sage-tooltip--is-open');
+    expect(overlay.getAttribute('aria-hidden')).toBe('false');
+    expect(overlay.getAttribute('aria-live')).toBe('polite');
 
     const c = await page.find('sage-tooltip');
     expect(await c.getProperty('opened')).toEqual(true);
