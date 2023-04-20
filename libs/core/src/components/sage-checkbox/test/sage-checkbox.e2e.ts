6ef581593aa0be2219decfa19f1ffebc462ac9e4
Phillip Lovelace <phillip@pixelflips.com>
2023-04-20 14:54:44 -0700

    style(checkbox): file formatting based on review

--- libs/core/src/components/sage-checkbox/test/sage-checkbox.e2e.ts
+++ libs/core/src/components/sage-checkbox/test/sage-checkbox.e2e.ts
@@ -4,40 +4,52 @@ describe('sage-checkbox', () => {
   it('toggles checked and unchecked', async () => {
     const page = await newE2EPage();
     await page.setContent('<sage-checkbox checkbox-id="default" label="Label text" />');
+
     const component = await page.find('sage-checkbox');
     expect(component).toHaveClass('hydrated');
+
     const checkbox = await page.find('sage-checkbox >>> input');
     await checkbox.click();
     expect(await checkbox.getProperty('checked')).toBeTruthy();
+
     await checkbox.click();
     expect(await checkbox.getProperty('checked')).toBeFalsy();
   });
+
   it('toggles input disabled state', async () => {
     const page = await newE2EPage();
     await page.setContent('<sage-checkbox checkbox-id="default" label="Label text" />');
     const component = await page.find('sage-checkbox');
     expect(component).toHaveClass('hydrated');
+
     let checkbox = await page.find('sage-checkbox >>> input');
     component.setProperty('disabled', false);
     await page.waitForChanges();
     expect(await checkbox.getProperty('disabled')).toBe(false);
+
     component.setProperty('disabled', true);
     await page.waitForChanges();
     expect(await checkbox.getProperty('disabled')).toBe(true);
   });
+
   it('emits "sageCheckboxChange" event when checkbox is changed', async () => {
     const page = await newE2EPage();
     await page.setContent('<sage-checkbox checkbox-id="default" label="Label text" />');
+
     const checkbox = await page.find('sage-checkbox >>> input');
     const eventSpy = await page.spyOnEvent('sageCheckboxChange');
     await checkbox.press('Space');
+
     expect(eventSpy).toHaveReceivedEvent();
   });
+
   it('does not emit "sageCheckboxChange" event when checkbox is changed and disabled', async () => {
     const page = await newE2EPage();
     await page.setContent('<sage-checkbox checkbox-id="default" label="Label text" disabled />');
+
     const checkbox = await page.find('sage-checkbox >>> input');
     const eventSpy = await page.spyOnEvent('sageCheckboxChange');
+
     await checkbox.press('Space');
     expect(eventSpy).not.toHaveReceivedEvent();
   });
--- libs/core/src/components/sage-checkbox/test/sage-checkbox.spec.tsx
+++ libs/core/src/components/sage-checkbox/test/sage-checkbox.spec.tsx
@@ -18,6 +18,7 @@ describe('sage-checkbox', () => {
       </sage-checkbox>
     `);
   });
+
   it('renders with id when componentId prop is set', async () => {
     const page = await newSpecPage({
       components: [SageCheckbox],
@@ -34,22 +35,27 @@ describe('sage-checkbox', () => {
       </sage-checkbox>
     `);
   });
+
   it('renders checked input when checked prop is set', async () => {
     const { root } = await newSpecPage({
       components: [SageCheckbox],
       html: `<sage-checkbox component-id="default" label="Label text" checked />`,
     });
+
     const input = root?.shadowRoot?.querySelector('input');
     expect(input?.checked).toBe(true);
   });
+
   it('renders disabled input when disabled prop is set', async () => {
     const { root } = await newSpecPage({
       components: [SageCheckbox],
       html: `<sage-checkbox component-id="default" label="Label text" disabled />`,
     });
+
     const input = root?.shadowRoot?.querySelector('input');
     expect(input?.disabled).toBe(true);
   });
+
   it('renders in invalid state when invalid prop is set', async () => {
     const page = await newSpecPage({
       components: [SageCheckbox],
@@ -66,6 +72,7 @@ describe('sage-checkbox', () => {
       </sage-checkbox>
     `);
   });
+
   it('renders in indeterminate state when indeterminate prop is set', async () => {
     const page = await newSpecPage({
       components: [SageCheckbox],
@@ -82,6 +89,7 @@ describe('sage-checkbox', () => {
       </sage-checkbox>
     `);
   });
+
   it('renders label text when label prop is set', async () => {
     const page = await newSpecPage({
       components: [SageCheckbox],
@@ -98,6 +106,7 @@ describe('sage-checkbox', () => {
       </sage-checkbox>
     `);
   });
+
   it('renders message when message prop is set', async () => {
     const page = await newSpecPage({
       components: [SageCheckbox],
@@ -115,44 +124,56 @@ describe('sage-checkbox', () => {
       </sage-checkbox>
     `);
   });
+
   it('renders required input when required prop is set', async () => {
     const { root } = await newSpecPage({
       components: [SageCheckbox],
       html: `<sage-checkbox component-id="default" label="Label text" required />`,
     });
+
     const input = root?.shadowRoot?.querySelector('input');
     expect(input?.required).toBe(true);
   });
+
   it('sets input value attribute when value prop is set', async () => {
     const { root } = await newSpecPage({
       components: [SageCheckbox],
       html: `<sage-checkbox component-id="default" label="Label text" value="This is the input value" />`,
     });
+
     const input = root?.shadowRoot?.querySelector('input');
     expect(input?.value).toEqual('This is the input value');
   });
+
   it('emits "sageCheckboxChange" event when checkbox is changed', async () => {
     const page = await newSpecPage({
       components: [SageCheckbox],
       html: '<sage-checkbox component-id="default" label="Label text" />',
     });
+
     const checkbox = page.root?.shadowRoot?.querySelector('input[type="checkbox"]');
     const eventSpy = jest.fn();
+
     page.root?.addEventListener('sageCheckboxChange', eventSpy);
     checkbox?.dispatchEvent(new Event('change'));
     await page.waitForChanges();
+
     expect(eventSpy).toHaveBeenCalled();
   });
+
   it('does not emit "sageCheckboxChange" event when checkbox is changed and disabled', async () => {
     const page = await newSpecPage({
       components: [SageCheckbox],
       html: '<sage-checkbox component-id="default" label="Label text" disabled />',
     });
+
     const checkbox = page.root?.shadowRoot?.querySelector('input[type="checkbox"]');
     const eventSpy = jest.fn();
+
     page.root?.addEventListener('sageCheckboxChange', eventSpy);
     checkbox?.dispatchEvent(new Event('change'));
     await page.waitForChanges();
+
     expect(eventSpy).not.toHaveBeenCalled();
   });
 });
