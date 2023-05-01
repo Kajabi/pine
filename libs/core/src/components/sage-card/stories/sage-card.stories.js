45934ff2c7d633f72af403f2b5c913dfec59a7bc
Phillip Lovelace <phillip@pixelflips.com>
2023-05-01 11:25:11 -0700

    docs(card): setup initial docs/stories

---
+++ libs/core/src/components/sage-card/stories/sage-card.stories.js
@@ -0,0 +1,19 @@
+import { html } from 'lit-html';
+
+const BaseTemplate = (args) =>
+  html` <sage-card></sage-card>`;
+
+const defaultParameters = { docs: { disable: true } };
+
+export const Default = BaseTemplate.bind();
+Default.args = {
+  compact: false,
+};
+Default.parameters = { ...defaultParameters };
+
+
+export const Compact = BaseTemplate.bind();
+Compact.args = {
+  compact: true,
+};
+Compact.parameters = { ...defaultParameters };
