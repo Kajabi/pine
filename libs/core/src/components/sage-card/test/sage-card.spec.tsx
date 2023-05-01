f500e7fa3cb634839e7188172cdbfde2939e0172
Phillip Lovelace <phillip@pixelflips.com>
2023-05-01 15:49:37 -0700

    test(card): add initial spec test

--- libs/core/src/components/sage-card/test/sage-card.spec.tsx
+++ libs/core/src/components/sage-card/test/sage-card.spec.tsx
@@ -7,8 +7,24 @@ describe('sage-card', () => {
       components: [SageCard],
       html: `<sage-card></sage-card>`,
     });
+
     expect(page.root).toEqualHtml(`
       <sage-card>
+        <mock:shadow-root>
+          <slot></slot>
+        </mock:shadow-root>
+      </sage-card>
+    `);
+  });
+
+  it('renders as compact when compact prop is set', async () => {
+    const page = await newSpecPage({
+      components: [SageCard],
+      html: `<sage-card compact></sage-card>`,
+    });
+
+    expect(page.root).toEqualHtml(`
+      <sage-card compact class="is-compact">
         <mock:shadow-root>
           <slot></slot>
         </mock:shadow-root>
