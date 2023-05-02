0e5d00ad7606ffd9723671be1b066c881290793e
Kevin Chang <kevin.chang@kajabi.com>
2023-05-02 16:00:44 -0700

    test(switch): more e2e tests

--- libs/core/src/components/sage-switch/test/sage-switch.e2e.ts
+++ libs/core/src/components/sage-switch/test/sage-switch.e2e.ts
@@ -7,58 +7,77 @@ describe('sage-switch', () => {
     const component = await page.find('sage-switch');
     expect(component).toHaveClass('hydrated');
 
-    const element = await page.find('sage-switch >>> input');
-    let value = await element.getProperty('checked');
+    const el = await page.find('sage-switch >>> input');
+    let value = await el.getProperty('checked');
     expect(value).toBe(false);
 
     component.setProperty('checked', 'true');
     await page.waitForChanges();
-    value = await element.getProperty('checked');
+    value = await el.getProperty('checked');
     expect(value).toBe(true);
   });
 
-  it('renders a disabled input', async () => {
+  it('renders a disabled input when toggled', async () => {
     const page = await newE2EPage();
     await page.setContent('<sage-switch></sage-switch>');
     const component = await page.find('sage-switch');
     expect(component).toHaveClass('hydrated');
 
-    const element = await page.find('sage-switch >>> input');
-    let value = await element.getProperty('disabled');
+    const el = await page.find('sage-switch >>> input');
+    let value = await el.getProperty('disabled');
     expect(value).toBe(false);
 
     component.setProperty('disabled', 'true');
     await page.waitForChanges();
-    value = await element.getProperty('disabled');
+    value = await el.getProperty('disabled');
     expect(value).toBe(true);
   });
 
-  it('renders an invalid input', async () => {
+  it('renders an invalid input when toggled', async () => {
     const page = await newE2EPage();
     await page.setContent('<sage-switch></sage-switch>');
     const component = await page.find('sage-switch');
     expect(component).toHaveClass('hydrated');
 
-    const element = await page.find('sage-switch >>> input');
-    let value = await element.getAttribute('aria-invalid');
+    const el = await page.find('sage-switch >>> input');
+    let value = el.getAttribute('aria-invalid');
     expect(value).toBe(null);
 
     component.setProperty('invalid', 'true');
     await page.waitForChanges();
-    value = await element.getAttribute('aria-invalid');
+    value = el.getAttribute('aria-invalid');
     expect(value).toBe("true");
   });
 
-  it('renders an invalid input with error message', async () => {
+  it('renders an invalid input with error message when toggled', async () => {
     const page = await newE2EPage();
     await page.setContent('<sage-switch invalid="true" error-message="Please correct this item"></sage-switch>');
     const component = await page.find('sage-switch');
     expect(component).toHaveClass('hydrated');
 
-    const element = await page.find('sage-switch >>> input');
-    const errorText = await page.find('sage-switch >>> .sage-switch__error-message');
-    const value = element.getAttribute('aria-invalid');
-    expect(value).toBe("true");
-    expect(errorText.textContent).toEqual(`Please correct this item`);
+    const el = await page.find('sage-switch >>> input');
+    const errText = await page.find('sage-switch >>> .sage-switch__error-message');
+    const ariaDesc = el.getAttribute('aria-describedby');
+    const ariaInvalid = el.getAttribute('aria-invalid');
+    expect(ariaInvalid).toBe("true");
+    expect(ariaDesc).toBe(null);
+    expect(errText.textContent).toEqual(`Please correct this item`);
+  });
+
+  it('renders a helper and error message and assigns the aria-description to the input', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<sage-switch component-id="switch-with-description" invalid="true" helper-message="This is a helper message" error-message="This is an error message"></sage-switch>');
+    const component = await page.find('sage-switch');
+    expect(component).toHaveClass('hydrated');
+
+    const el = await page.find('sage-switch >>> input');
+    const helperMessage = await page.find('sage-switch >>> #switch-with-description__helper-message');
+    const errorMessage = await page.find('sage-switch >>> #switch-with-description__error-message');
+    const ariaDesc = el.getAttribute('aria-describedby');
+    const ariaInvalid = el.getAttribute('aria-invalid');
+    expect(ariaInvalid).toBe("true");
+    expect(ariaDesc).toBe('switch-with-description__error-message');
+    expect(helperMessage.textContent).toEqual(`This is a helper message`);
+    expect(errorMessage.textContent).toEqual(`This is an error message`);
   });
 });
