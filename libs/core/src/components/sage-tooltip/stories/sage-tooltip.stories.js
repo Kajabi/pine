2dadafa0ac03d7fa02692f19e026052042bb542b
Quinton Jason <quinton.jason@gmail.com>
2023-05-02 16:35:56 -0500

    docs(tooltip): update to placement demo indentation

--- libs/core/src/components/sage-tooltip/stories/sage-tooltip.stories.js
+++ libs/core/src/components/sage-tooltip/stories/sage-tooltip.stories.js
@@ -20,51 +20,52 @@ const HTMLContentTemplate = (args) => html`
 
 const PositionTemplate = (args) => html`
 <div class="demo-container" style="min-height: 50vh; width: 100%; display: flex; align-items: center; justify-content: center;">
-<div class="position-demo-grid">
-<div class="position-demo-grid-item"><sage-tooltip content="content 2" has-arrow=${args.hasArrow} placement="top-start" opened=${args.opened}>
-  <sage-button variant="accent">t</sage-button>
-</sage-tooltip>
-<sage-tooltip content="content 2" has-arrow=${args.hasArrow} placement="top" opened=${args.opened}>
-  <sage-button variant="accent">t</sage-button>
-</sage-tooltip>
-<sage-tooltip content="content 3" has-arrow=${args.hasArrow} placement="top-end" opened=${args.opened}>
-  <sage-button variant="accent">te</sage-button>
-</sage-tooltip>
-</div>
-<div class="position-demo-grid-item">
-<sage-tooltip content="content 1" has-arrow=${args.hasArrow} placement="left-start" opened=${args.opened}>
-  <sage-button variant="accent">ls</sage-button>
-</sage-tooltip>
-<sage-tooltip content="content 2" has-arrow=${args.hasArrow} placement="left" opened=${args.opened}>
-  <sage-button variant="accent">l</sage-button>
-</sage-tooltip>
-<sage-tooltip content="content 3" has-arrow=${args.hasArrow} placement="left-end" opened=${args.opened}>
-  <sage-button variant="accent">le</sage-button>
-</sage-tooltip>
-</div>
-<div class="position-demo-grid-item">
-<sage-tooltip content="content 1" has-arrow=${args.hasArrow} placement="bottom-start" opened=${args.opened}>
-  <sage-button variant="accent">bs</sage-button>
-</sage-tooltip>
-<sage-tooltip content="content 2" has-arrow=${args.hasArrow} placement="bottom" opened=${args.opened}>
-  <sage-button variant="accent">b</sage-button>
-</sage-tooltip>
-<sage-tooltip content="content 3" has-arrow=${args.hasArrow} placement="bottom-end" opened=${args.opened}>
-  <sage-button variant="accent">be</sage-button>
-</sage-tooltip>
-</div>
-<div class="position-demo-grid-item">
-<sage-tooltip content="content 1" has-arrow=${args.hasArrow} placement="right-start" opened=${args.opened}>
-  <sage-button variant="accent">rs</sage-button>
-</sage-tooltip>
-<sage-tooltip content="content 2" has-arrow=${args.hasArrow} placement="right" opened=${args.opened}>
-  <sage-button variant="accent">r</sage-button>
-</sage-tooltip>
-<sage-tooltip content="content 3" has-arrow=${args.hasArrow} placement="right-end" opened=${args.opened}>
-  <sage-button variant="accent">re</sage-button>
-</sage-tooltip>
-</div>
-</div>
+  <div class="position-demo-grid">
+    <div class="position-demo-grid-item">
+      <sage-tooltip content="content 2" has-arrow=${args.hasArrow} placement="top-start" opened=${args.opened}>
+        <sage-button variant="accent">t</sage-button>
+      </sage-tooltip>
+      <sage-tooltip content="content 2" has-arrow=${args.hasArrow} placement="top" opened=${args.opened}>
+        <sage-button variant="accent">t</sage-button>
+      </sage-tooltip>
+      <sage-tooltip content="content 3" has-arrow=${args.hasArrow} placement="top-end" opened=${args.opened}>
+        <sage-button variant="accent">te</sage-button>
+      </sage-tooltip>
+      </div>
+      <div class="position-demo-grid-item">
+        <sage-tooltip content="content 1" has-arrow=${args.hasArrow} placement="left-start" opened=${args.opened}>
+          <sage-button variant="accent">ls</sage-button>
+        </sage-tooltip>
+        <sage-tooltip content="content 2" has-arrow=${args.hasArrow} placement="left" opened=${args.opened}>
+          <sage-button variant="accent">l</sage-button>
+        </sage-tooltip>
+        <sage-tooltip content="content 3" has-arrow=${args.hasArrow} placement="left-end" opened=${args.opened}>
+          <sage-button variant="accent">le</sage-button>
+        </sage-tooltip>
+      </div>
+      <div class="position-demo-grid-item">
+        <sage-tooltip content="content 1" has-arrow=${args.hasArrow} placement="bottom-start" opened=${args.opened}>
+          <sage-button variant="accent">bs</sage-button>
+        </sage-tooltip>
+        <sage-tooltip content="content 2" has-arrow=${args.hasArrow} placement="bottom" opened=${args.opened}>
+          <sage-button variant="accent">b</sage-button>
+        </sage-tooltip>
+        <sage-tooltip content="content 3" has-arrow=${args.hasArrow} placement="bottom-end" opened=${args.opened}>
+          <sage-button variant="accent">be</sage-button>
+        </sage-tooltip>
+      </div>
+      <div class="position-demo-grid-item">
+        <sage-tooltip content="content 1" has-arrow=${args.hasArrow} placement="right-start" opened=${args.opened}>
+          <sage-button variant="accent">rs</sage-button>
+        </sage-tooltip>
+        <sage-tooltip content="content 2" has-arrow=${args.hasArrow} placement="right" opened=${args.opened}>
+          <sage-button variant="accent">r</sage-button>
+        </sage-tooltip>
+        <sage-tooltip content="content 3" has-arrow=${args.hasArrow} placement="right-end" opened=${args.opened}>
+          <sage-button variant="accent">re</sage-button>
+        </sage-tooltip>
+    </div>
+  </div>
 </div>`;
 
 export const Default = BaseTemplate.bind({});
