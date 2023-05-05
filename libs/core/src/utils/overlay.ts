84f2c5a31e3b5ef0f8c8b85b4e1a4187e224061b
Quinton Jason <quinton.jason@gmail.com>
2023-05-05 11:51:15 -0500

    test(tooltip): update test expectation and update if to switch

--- libs/core/src/utils/overlay.ts
+++ libs/core/src/utils/overlay.ts
@@ -7,70 +7,76 @@ export const positionTooltip = (elem, elemPlacement, overlay, offset = 8) => {
   // Exit the function if the placement is not set
   if (!elemPlacement || elemPlacement == "") return;
 
-  if (elemPlacement.includes("right")) {
-    overlay.style.top = '50%';
-    overlay.style.left = `calc(${rect.width}px + ${offset}px)`;
-    overlay.style.transform = 'translateY(-50%)';
+  switch (true) {
+    case elemPlacement.includes("right"):
+      overlay.style.top = '50%';
+      overlay.style.left = `calc(${rect.width}px + ${offset}px)`;
+      overlay.style.transform = 'translateY(-50%)';
 
-    if (elemPlacement.includes("start")) {
-      overlay.style.top = '0';
-      overlay.style.transform = 'translateY(0)';
-    }
+      if (elemPlacement.includes("start")) {
+        overlay.style.top = '0';
+        overlay.style.transform = 'translateY(0)';
+      }
 
-    if (elemPlacement.includes("end")) {
-      overlay.style.bottom = '0';
-      overlay.style.top = 'initial';
-      overlay.style.transform = 'translateY(0)';
-    }
-  }
+      if (elemPlacement.includes("end")) {
+        overlay.style.bottom = '0';
+        overlay.style.top = 'initial';
+        overlay.style.transform = 'translateY(0)';
+      }
 
-  if (elemPlacement.includes("left")) {
-    overlay.style.top = '50%';
-    overlay.style.right = `calc(${rect.width}px + ${offset}px)`;
-    overlay.style.transform = 'translateY(-50%)';
+      break;
 
-    if (elemPlacement.includes("start")) {
-      overlay.style.top = '0';
-      overlay.style.transform = 'translateY(0)';
-    }
+    case elemPlacement.includes("left"):
+      overlay.style.top = '50%';
+      overlay.style.right = `calc(${rect.width}px + ${offset}px)`;
+      overlay.style.transform = 'translateY(-50%)';
 
-    if (elemPlacement.includes("end")) {
-      overlay.style.bottom = '0';
-      overlay.style.top = 'initial';
-      overlay.style.transform = 'translateY(0)';
-    }
-  }
+      if (elemPlacement.includes("start")) {
+        overlay.style.top = '0';
+        overlay.style.transform = 'translateY(0)';
+      }
 
-  if (elemPlacement.includes("bottom")) {
-    overlay.style.top = `calc(${rect.height}px + ${offset}px)`;
-    overlay.style.left = '50%';
-    overlay.style.transform = 'translateX(-50%)';
+      if (elemPlacement.includes("end")) {
+        overlay.style.bottom = '0';
+        overlay.style.top = 'initial';
+        overlay.style.transform = 'translateY(0)';
+      }
 
-    if (elemPlacement.includes("start")) {
-      overlay.style.left = '0';
-      overlay.style.transform = 'translateX(0)';
-    }
+      break;
 
-    if (elemPlacement.includes("end")) {
-      overlay.style.left = 'initial';
-      overlay.style.right = '0';
-      overlay.style.transform = 'translateX(0)';
-    }
-  }
+    case elemPlacement.includes("bottom"):
+      overlay.style.top = `calc(${rect.height}px + ${offset}px)`;
+      overlay.style.left = '50%';
+      overlay.style.transform = 'translateX(-50%)';
 
-  if (elemPlacement.includes("top")) {
-    overlay.style.top = `calc((${contentRect.height}px + ${offset}px) * -1)`;
-    overlay.style.left = '50%';
-    overlay.style.transform = 'translateX(-50%)';
+      if (elemPlacement.includes("start")) {
+        overlay.style.left = '0';
+        overlay.style.transform = 'translateX(0)';
+      }
 
-    if (elemPlacement.includes("start")) {
-      overlay.style.left = '0';
-      overlay.style.transform = 'translateX(0)';
-    }
-    if (elemPlacement.includes("end")) {
-      overlay.style.left = 'initial';
-      overlay.style.right = '0';
-      overlay.style.transform = 'translateX(0)';
-    }
+      if (elemPlacement.includes("end")) {
+        overlay.style.left = 'initial';
+        overlay.style.right = '0';
+        overlay.style.transform = 'translateX(0)';
+      }
+
+      break;
+
+    case elemPlacement.includes("top"):
+      overlay.style.top = `calc((${contentRect.height}px + ${offset}px) * -1)`;
+      overlay.style.left = '50%';
+      overlay.style.transform = 'translateX(-50%)';
+
+      if (elemPlacement.includes("start")) {
+        overlay.style.left = '0';
+        overlay.style.transform = 'translateX(0)';
+      }
+      if (elemPlacement.includes("end")) {
+        overlay.style.left = 'initial';
+        overlay.style.right = '0';
+        overlay.style.transform = 'translateX(0)';
+      }
+
+      break;
   }
 }
