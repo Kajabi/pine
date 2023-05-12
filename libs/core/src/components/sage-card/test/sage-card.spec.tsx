4eb73f54dd73c46a93807a088bbdc2ed727fab29
Phillip Lovelace <phillip@pixelflips.com>
2023-05-12 11:36:00 -0700

    test(card): add additional spec tests for coverage

--- libs/core/src/components/sage-card/test/sage-card.spec.tsx
+++ libs/core/src/components/sage-card/test/sage-card.spec.tsx
@@ -16,4 +16,64 @@ describe('sage-card', () => {
       </sage-card>
     `);
   });
+
+  it('renders with set padding when padding prop is set', async () => {
+    const page = await newSpecPage({
+      components: [SageCard],
+      html: `<sage-card padding="sm"></sage-card>`,
+    });
+
+    expect(page.root).toEqualHtml(`
+    <sage-card padding="sm" class="sage-card has-border sage-card--padding-sm sage-card--shadow-none">
+      <mock:shadow-root>
+        <slot></slot>
+      </mock:shadow-root>
+    </sage-card>
+    `);
+  });
+
+  it('renders with no border when border is false', async () => {
+    const page = await newSpecPage({
+      components: [SageCard],
+      html: `<sage-card border="false"></sage-card>`,
+    });
+
+    expect(page.root).toEqualHtml(`
+    <sage-card border="false" class="sage-card sage-card--padding-md sage-card--shadow-none">
+      <mock:shadow-root>
+        <slot></slot>
+      </mock:shadow-root>
+    </sage-card>
+    `);
+  });
+
+  it('renders with set shadow when shadow prop is set', async () => {
+    const page = await newSpecPage({
+      components: [SageCard],
+      html: `<sage-card shadow="sm"></sage-card>`,
+    });
+
+    expect(page.root).toEqualHtml(`
+    <sage-card shadow="sm" class="sage-card has-border sage-card--padding-md sage-card--shadow-sm">
+      <mock:shadow-root>
+        <slot></slot>
+      </mock:shadow-root>
+    </sage-card>
+    `);
+  });
+
+  it('renders with background color when bg-color prop is set', async () => {
+    const page = await newSpecPage({
+      components: [SageCard],
+      html: `<sage-card bg-color="#CCCCCC" border="false"></sage-card>`,
+    });
+
+    expect(page.root).toEqualHtml(`
+    <sage-card bg-color="#CCCCCC" border="false" class="sage-card sage-card--padding-md sage-card--shadow-none" style="background-color: #CCCCCC;">
+      <mock:shadow-root>
+        <slot></slot>
+      </mock:shadow-root>
+    </sage-card>
+    `);
+  });
 });
