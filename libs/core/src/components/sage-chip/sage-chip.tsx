d9f5ffdd2701f8514fa9ef1d3ee8df9442858640
Phillip Lovelace <phillip@pixelflips.com>
2023-05-19 15:39:37 -0700

    fix(chip): convert dropdown to button and correct styling

--- libs/core/src/components/sage-chip/sage-chip.tsx
+++ libs/core/src/components/sage-chip/sage-chip.tsx
@@ -12,16 +12,24 @@ export class SageChip {
   @Prop() variant: 'text' | 'tag' | 'dropdown' = 'text';
 
   render() {
+    const isDropdown = this.variant === 'dropdown';
+    const chipLabel = isDropdown ? (
+      <button class="sage-chip__button" type="button">
+        {this.label}
+        <sage-icon name="caret-down" size="10px"></sage-icon>
+      </button>
+    ) : (
+      <span class="sage-chip__text">{this.label}</span>
+    );
+
     return (
       <Host class={`sage-chip sage-chip--${this.variant} sage-chip--${this.color}`}>
         {this.status && <i class="sage-chip__status" aria-hidden="true"></i>}
-        {this.variant !== 'dropdown' && <span class="sage-chip__text">{this.label}</span>}
-        {this.variant === 'tag' && <button class="sage-chip__close"><sage-icon name="remove" size="12px"></sage-icon></button>}
-        {this.variant === 'dropdown' && (
-          <span class="sage-chip__dropdown">
-            {this.label}
-            <sage-icon name="caret-down" size="10px"></sage-icon>
-          </span>
+        {chipLabel}
+        {this.variant === 'tag' && (
+          <button class="sage-chip__close" type="button">
+            <sage-icon name="remove" size="12px"></sage-icon>
+          </button>
         )}
       </Host>
     );
