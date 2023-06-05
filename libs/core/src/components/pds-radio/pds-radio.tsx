15b9d984d103dd4e51dbf0c21989b7cfd3c6a629
Phillip Lovelace <phillip@pixelflips.com>
2023-06-05 15:25:07 -0700

    feat(radio): initial markup and story file creation

--- libs/core/src/components/pds-radio/pds-radio.tsx
+++ libs/core/src/components/pds-radio/pds-radio.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, h, Prop } from '@stencil/core';
 
 @Component({
   tag: 'pds-radio',
@@ -6,13 +6,80 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class PdsRadio {
+  /**
+   * Determines whether or not the radio is checked.
+   */
+  @Prop() checked: boolean;
+
+  /**
+   * Determines whether or not the radio is disabled.
+   */
+  @Prop() disabled: boolean;
+
+  /**
+   * It determines whether or not the radio is invalid.
+   */
+  @Prop() invalid: boolean;
+
+  /**
+   * String used for radio `id` attribute and label `for` attribute.
+   */
+  @Prop() componentId: string;
+
+  /**
+   * String used for label text next to radio.
+   */
+  @Prop() label: string;
+
+  /**
+   * String used for helper message below radio.
+   */
+  @Prop() helperMessage: string;
+
+  /**
+   * String used for radio `name` attribute.
+   */
+  @Prop() name: string;
+
+  /**
+   * It determines whether or not the radio is required.
+   */
+  @Prop() required: boolean;
+
+  /**
+   * The value of the radio that is submitted with a form.
+   */
+  @Prop() value: string;
+
+  private classNames() {
+    const classNames = [];
+
+    if (this.invalid) {
+      classNames.push('is-invalid');
+    }
+    if (this.disabled) {
+      classNames.push('is-disabled');
+    }
+
+    return classNames.join('  ');
+  }
 
   render() {
     return (
-      <Host>
-        <slot></slot>
+      <Host class={this.classNames()}>
+        <input
+          type="radio"
+          id={this.componentId}
+          name={this.name}
+          value={this.value}
+          checked={this.checked}
+          required={this.required}
+          disabled={this.disabled}
+          // onChange={(event) => this.handleRadioChange(event)}
+        />
+        <label htmlFor={this.componentId}>{this.label}</label>
+        {this.helperMessage && <div class={'sage-radio__message'}>{this.helperMessage}</div>}
       </Host>
     );
   }
-
 }
--- libs/core/src/components/pds-radio/readme.md
+++ libs/core/src/components/pds-radio/readme.md
@@ -5,6 +5,21 @@
 <!-- Auto Generated Below -->
 
 
+## Properties
+
+| Property        | Attribute        | Description                                                     | Type      | Default     |
+| --------------- | ---------------- | --------------------------------------------------------------- | --------- | ----------- |
+| `checked`       | `checked`        | Determines whether or not the radio is checked.                 | `boolean` | `undefined` |
+| `componentId`   | `component-id`   | String used for radio `id` attribute and label `for` attribute. | `string`  | `undefined` |
+| `disabled`      | `disabled`       | Determines whether or not the radio is disabled.                | `boolean` | `undefined` |
+| `helperMessage` | `helper-message` | String used for helper message below radio.                     | `string`  | `undefined` |
+| `invalid`       | `invalid`        | It determines whether or not the radio is invalid.              | `boolean` | `undefined` |
+| `label`         | `label`          | String used for label text next to radio.                       | `string`  | `undefined` |
+| `name`          | `name`           | String used for radio `name` attribute.                         | `string`  | `undefined` |
+| `required`      | `required`       | It determines whether or not the radio is required.             | `boolean` | `undefined` |
+| `value`         | `value`          | The value of the radio that is submitted with a form.           | `string`  | `undefined` |
+
+
 ----------------------------------------------
 
 
