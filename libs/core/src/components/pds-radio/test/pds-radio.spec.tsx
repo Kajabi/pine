143d843a5bd36fd705f9c665d59a9f39cdf7d7c2
Phillip Lovelace <phillip@pixelflips.com>
2023-06-06 14:12:40 -0700

    test(radio): add spec tests excluding events

--- libs/core/src/components/pds-radio/test/pds-radio.spec.tsx
+++ libs/core/src/components/pds-radio/test/pds-radio.spec.tsx
@@ -17,4 +17,110 @@ describe('pds-radio', () => {
       </pds-radio>
     `);
   });
+
+  it('renders with id when componentId prop is set', async () => {
+    const page = await newSpecPage({
+      components: [SageRadio],
+      html: `<sage-radio component-id="default" label="Label text" />`,
+    });
+
+    expect(page.root).toEqualHtml(`
+      <sage-radio component-id="default" label="Label text">
+        <mock:shadow-root>
+          <input type="radio" id="default">
+          <label htmlfor="default">Label text</label>
+        </mock:shadow-root>
+      </sage-radio>
+    `);
+  });
+
+  it('renders checked input when checked prop is set', async () => {
+    const { root } = await newSpecPage({
+      components: [SageRadio],
+      html: `<sage-radio component-id="default" label="Label text" checked />`,
+    });
+
+    const input = root?.shadowRoot?.querySelector('input');
+    expect(input?.checked).toBe(true);
+  });
+
+  it('renders disabled input when disabled prop is set', async () => {
+    const { root } = await newSpecPage({
+      components: [SageRadio],
+      html: `<sage-radio component-id="default" label="Label text" disabled />`,
+    });
+
+    const input = root?.shadowRoot?.querySelector('input');
+    expect(input?.disabled).toBe(true);
+  });
+
+  it('renders in invalid state when invalid prop is set', async () => {
+    const page = await newSpecPage({
+      components: [SageRadio],
+      html: `<sage-radio component-id="default" label="Label text" invalid />`,
+    });
+
+    expect(page.root).toEqualHtml(`
+      <sage-radio class="is-invalid" component-id="default" label="Label text" invalid>
+        <mock:shadow-root>
+          <input type="radio" id="default">
+          <label htmlfor="default">Label text</label>
+        </mock:shadow-root>
+      </sage-radio>
+    `);
+  });
+
+  it('renders label text when label prop is set', async () => {
+    const page = await newSpecPage({
+      components: [SageRadio],
+      html: `<sage-radio component-id="default" label="This is label text" />`,
+    });
+
+    expect(page.root).toEqualHtml(`
+      <sage-radio component-id="default" label="This is label text">
+        <mock:shadow-root>
+          <input type="radio" id="default">
+          <label htmlfor="default">This is label text</label>
+        </mock:shadow-root>
+      </sage-radio>
+    `);
+  });
+
+  it('renders message when message prop is set', async () => {
+    const page = await newSpecPage({
+      components: [SageRadio],
+      html: `<sage-radio component-id="default" label="Label text" helper-message="This is short message text." />`,
+    });
+
+    expect(page.root).toEqualHtml(`
+      <sage-radio component-id="default" label="Label text" helper-message="This is short message text.">
+        <mock:shadow-root>
+          <input type="radio" id="default">
+          <label htmlfor="default">Label text</label>
+          <div class="sage-radio__message">This is short message text.</div>
+        </mock:shadow-root>
+      </sage-radio>
+    `);
+  });
+
+  it('renders required input when required prop is set', async () => {
+    const { root } = await newSpecPage({
+      components: [SageRadio],
+      html: `<sage-radio component-id="default" label="Label text" required />`,
+    });
+
+    const input = root?.shadowRoot?.querySelector('input');
+    expect(input?.required).toBe(true);
+  });
+
+  it('sets input value attribute when value prop is set', async () => {
+    const { root } = await newSpecPage({
+      components: [SageRadio],
+      html: `<sage-radio component-id="default" label="Label text" value="This is the input value" />`,
+    });
+
+    const input = root?.shadowRoot?.querySelector('input');
+    expect(input?.value).toEqual('This is the input value');
+  });
+
 });
