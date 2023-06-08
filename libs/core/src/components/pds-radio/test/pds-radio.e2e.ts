502eed8462abefec6e2d32f49a40a6ee3427899d
Phillip Lovelace <phillip@pixelflips.com>
2023-06-08 14:37:21 -0700

    test(radio): update tests after changing to scoped

--- libs/core/src/components/pds-radio/test/pds-radio.e2e.ts
+++ libs/core/src/components/pds-radio/test/pds-radio.e2e.ts
@@ -16,7 +16,7 @@ describe('pds-radio', () => {
     const component = await page.find('pds-radio');
     expect(component).toHaveClass('hydrated');
 
-    const radio = await page.find('pds-radio >>> input');
+    const radio = await page.find('pds-radio input');
     await radio.click();
     expect(await radio.getProperty('checked')).toBeTruthy();
   });
@@ -27,7 +27,7 @@ describe('pds-radio', () => {
     const component = await page.find('pds-radio');
     expect(component).toHaveClass('hydrated');
 
-    let radio = await page.find('pds-radio >>> input');
+    let radio = await page.find('pds-radio input');
     component.setProperty('disabled', false);
     await page.waitForChanges();
     expect(await radio.getProperty('disabled')).toBe(false);
@@ -37,23 +37,23 @@ describe('pds-radio', () => {
     expect(await radio.getProperty('disabled')).toBe(true);
   });
 
-  it('emits "sageRadioChange" event when radio is checked', async () => {
+  it('emits "pdsRadioChange" event when radio is checked', async () => {
     const page = await newE2EPage();
     await page.setContent('<pds-radio component-id="default" label="Label text" />');
 
-    const radio = await page.find('pds-radio >>> input');
-    const eventSpy = await page.spyOnEvent('sageRadioChange');
+    const radio = await page.find('pds-radio input');
+    const eventSpy = await page.spyOnEvent('pdsRadioChange');
     await radio.press('Space');
 
     expect(eventSpy).toHaveReceivedEvent();
   });
 
-  it('does not emit "sageRadioChange" event when radio is clicked and disabled', async () => {
+  it('does not emit "pdsRadioChange" event when radio is clicked and disabled', async () => {
     const page = await newE2EPage();
     await page.setContent('<pds-radio component-id="default" label="Label text" disabled />');
 
-    const radio = await page.find('pds-radio >>> input');
-    const eventSpy = await page.spyOnEvent('sageRadioChange');
+    const radio = await page.find('pds-radio input');
+    const eventSpy = await page.spyOnEvent('pdsRadioChange');
 
     await radio.press('Space');
     expect(eventSpy).not.toHaveReceivedEvent();
