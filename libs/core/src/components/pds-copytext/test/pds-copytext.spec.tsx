1cbb671c98dc79347cfa1ced07359cd9a44a90e9
Phillip Lovelace <phillip@pixelflips.com>
2023-07-11 15:39:39 -0700

    test(copytext): update tests

--- libs/core/src/components/pds-copytext/test/pds-copytext.spec.tsx
+++ libs/core/src/components/pds-copytext/test/pds-copytext.spec.tsx
@@ -10,12 +10,96 @@ describe('pds-copytext', () => {
     expect(page.root).toEqualHtml(`
       <pds-copytext class="pds-copytext pds-copytext--bordered">
         <mock:shadow-root>
-          <button type="button">
+          <pds-button type="button" variant="unstyled">
             <span></span>
             <pds-icon name="copy" size="16px"></pds-icon>
-          </button>
+          </pds-button>
         </mock:shadow-root>
       </pds-copytext>
     `);
   });
+
+  it('renders without border when border prop is false', async () => {
+    const page = await newSpecPage({
+      components: [PdsCopytext],
+      html: `<pds-copytext border="false"></pds-copytext>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <pds-copytext class="pds-copytext" border="false">
+        <mock:shadow-root>
+          <pds-button type="button" variant="unstyled">
+            <span></span>
+            <pds-icon name="copy" size="16px"></pds-icon>
+          </pds-button>
+        </mock:shadow-root>
+      </pds-copytext>
+    `);
+  });
+
+  it('renders full width when full-width prop is set', async () => {
+    const page = await newSpecPage({
+      components: [PdsCopytext],
+      html: `<pds-copytext full-width="true"></pds-copytext>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <pds-copytext class="pds-copytext pds-copytext--bordered pds-copytext--full-width" full-width="true">
+        <mock:shadow-root>
+          <pds-button type="button" variant="unstyled">
+            <span></span>
+            <pds-icon name="copy" size="16px"></pds-icon>
+          </pds-button>
+        </mock:shadow-root>
+      </pds-copytext>
+    `);
+  });
+
+  it('renders truncated when trucate prop is set', async () => {
+    const page = await newSpecPage({
+      components: [PdsCopytext],
+      html: `<pds-copytext truncate="true"></pds-copytext>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <pds-copytext class="pds-copytext pds-copytext--bordered pds-copytext--truncated" truncate="true">
+        <mock:shadow-root>
+          <pds-button type="button" variant="unstyled">
+            <span></span>
+            <pds-icon name="copy" size="16px"></pds-icon>
+          </pds-button>
+        </mock:shadow-root>
+      </pds-copytext>
+    `);
+  });
+
+  it('renders value text when value prop is set', async () => {
+    const page = await newSpecPage({
+      components: [PdsCopytext],
+      html: `<pds-copytext value="custom value text"></pds-copytext>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <pds-copytext class="pds-copytext pds-copytext--bordered" value="custom value text">
+        <mock:shadow-root>
+          <pds-button type="button" variant="unstyled">
+            <span>custom value text</span>
+            <pds-icon name="copy" size="16px"></pds-icon>
+          </pds-button>
+        </mock:shadow-root>
+      </pds-copytext>
+    `);
+  });
+
+  it('calls copyToClipboard when the button is clicked', async () => {
+    const page = await newSpecPage({
+      components: [PdsCopytext],
+      html: `<pds-copytext value="Test Value"></pds-copytext>`,
+    });
+
+    const component = page.rootInstance as PdsCopytext;
+    spyOn(component, 'copyToClipboard');
+
+    const button = page.root?.shadowRoot?.querySelector('pds-button');
+    button?.dispatchEvent(new Event('click'));
+
+    expect(component.copyToClipboard).toHaveBeenCalledWith('Test Value');
+  });
+
 });
