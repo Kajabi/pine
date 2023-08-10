87fd8e476f04105a262aaebb23eb66e3699697d3
Quinton Jason <quinton.jason@gmail.com>
2023-08-09 20:13:14 -0500

    docs(pds-select): update stories and docs

--- libs/core/src/components/pds-select/stories/pds-select.stories.js
+++ libs/core/src/components/pds-select/stories/pds-select.stories.js
@@ -12,18 +12,18 @@ const BaseTemplate = (args) => html`
 
 // TODO: This example is to handle cases when the value is different from the pds-select-option text content
 // const BaseTemplate = (args) => html`
-//   <pds-select component-id="combobox" label="Label">
-//     <pds-select-option component-id="opt0" value="val0">Select an Option</pds-select-option>
-//     <pds-select-option component-id="opt1" value="val1">Option A value</pds-select-option>
-//     <pds-select-option component-id="opt2" value="val2">Option B value</pds-select-option>
-//     <pds-select-option component-id="opt3" value="val3">Option C value</pds-select-option>
-//     <pds-select-option component-id="opt4" value="val4">
-//       <div>
-//         <p>Option D</p>
-//         <p>value</p>
-//       </div>
-//     </pds-select-option>
-//   </pds-select>
+  // <pds-select component-id="combobox" label="Label">
+  //   <pds-select-option component-id="opt0" value="val0">Select an Option</pds-select-option>
+  //   <pds-select-option component-id="opt1" value="val1">Option A value</pds-select-option>
+  //   <pds-select-option component-id="opt2" value="val2">Option B value</pds-select-option>
+  //   <pds-select-option component-id="opt3" value="val3">Option C value</pds-select-option>
+  //   <pds-select-option component-id="opt4" value="val4">
+  //     <div>
+  //       <p>Option D</p>
+  //       <p>value</p>
+  //     </div>
+  //   </pds-select-option>
+  // </pds-select>
 // `;
 
 // This example is to handle cases when the value is not set, but the pds-select-option text content is present
