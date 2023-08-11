37534db2db398fc88974678985a7c9013fccb475
Quinton Jason <quinton.jason@gmail.com>
2023-08-11 11:49:55 -0500

    docs(pds-select): copy updates and comment removal

--- libs/core/src/components/pds-select/stories/pds-select.stories.js
+++ libs/core/src/components/pds-select/stories/pds-select.stories.js
@@ -10,35 +10,6 @@ const BaseTemplate = (args) => html`
   </pds-select>
 `;
 
-// TODO: This example is to handle cases when the value is different from the pds-select-option text content
-// const BaseTemplate = (args) => html`
-  // <pds-select component-id="combobox" label="Label">
-  //   <pds-select-option component-id="opt0" value="val0">Select an Option</pds-select-option>
-  //   <pds-select-option component-id="opt1" value="val1">Option A value</pds-select-option>
-  //   <pds-select-option component-id="opt2" value="val2">Option B value</pds-select-option>
-  //   <pds-select-option component-id="opt3" value="val3">Option C value</pds-select-option>
-  //   <pds-select-option component-id="opt4" value="val4">
-  //     <div>
-  //       <p>Option D</p>
-  //       <p>value</p>
-  //     </div>
-  //   </pds-select-option>
-  // </pds-select>
-// `;
-
-// This example is to handle cases when the value is not set, but the pds-select-option text content is present
-
-// NOTE FOR DOCS - id on select and option are both needed for accessibility regarding aria-activedescendant
-// const BaseTemplate = (args) => html`
-//   <pds-select component-id="combobox" label="Label">
-//     <pds-select-option component-id="opt0">Select an option</pds-select-option>
-//     <pds-select-option component-id="opt1">Option A Slot</pds-select-option>
-//     <pds-select-option component-id="opt2">Option B Slot</pds-select-option>
-//     <pds-select-option component-id="opt3">Option C Slot</pds-select-option>
-//     <pds-select-option component-id="opt4">Option D Slot</pds-select-option>
-//   </pds-select>
-// `;
-
 const defaultParameters = {
   docs: {
     disable: true,
@@ -61,12 +32,3 @@ Default.args = {
   name: 'Default',
 };
 Default.parameters = { ...defaultParameters };
-
-// export const SlottedOptions = OptionsSlotTemplate.bind({});
-// SlottedOptions.args = {
-//   componentId: 'pds-select-options-slot-example',
-//   label: 'Name',
-//   onChange: selectEventExample(),
-//   name: 'Slotted Options',
-// };
-// SlottedOptions.parameters = { ...defaultParameters };
