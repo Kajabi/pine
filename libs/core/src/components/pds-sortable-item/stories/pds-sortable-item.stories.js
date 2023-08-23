4675051b08e51f464fb536c4de2bfac5cd578811
Phillip Lovelace <phillip@pixelflips.com>
2023-08-23 12:44:59 -0700

    docs(sortable): add the items table to the docs

---
+++ libs/core/src/components/pds-sortable-item/stories/pds-sortable-item.stories.js
@@ -0,0 +1,23 @@
+import { html } from 'lit-html';
+import { extractArgTypes } from '@pxtrn/storybook-addon-docs-stencil';
+
+export default {
+  argTypes: extractArgTypes('pds-sortable-item'),
+  component: 'pds-sortable-item',
+  title: 'components/Sortable/Sortable Item',
+};
+
+const BaseTemplate = (args) => html`
+<pds-sortable-item
+  action="${args.actions}"
+  component-id="${args.componentId}
+  handle="${args.handle}"
+>
+</pds-sortable-item>`;
+
+export const Default = BaseTemplate.bind();
+Default.args = {
+  componentId: 'default',
+  actions: false,
+  handle: false,
+};
