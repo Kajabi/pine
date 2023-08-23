8a8ffa6e1375f20fcb8b7a3a605e775287b68af1
Phillip Lovelace <phillip@pixelflips.com>
2023-08-23 14:57:58 -0700

    docs(sortable): update story templates

--- libs/core/src/components/pds-sortable/stories/pds-sortable.stories.js
+++ libs/core/src/components/pds-sortable/stories/pds-sortable.stories.js
@@ -4,16 +4,74 @@ import { extractArgTypes } from '@pxtrn/storybook-addon-docs-stencil';
 export default {
   argTypes: extractArgTypes('pds-sortable'),
   component: 'pds-sortable',
-  title: 'components/Sortable'
-}
+  title: 'components/Sortable',
+};
 
 const BaseTemplate = (args) => html`
-<pds-sortable
-  border="${args.border}"
-  component-id="${args.componentId}
-  dividers="${args.dividers}"
-  handle-type="${args.handleType}"
->
+<pds-sortable border="${args.border}" component-id="${args.componentId}" dividers="${args.dividers}" handle-type="${args.handleType}">
+  <pds-sortable-item>Item 1</pds-sortable-item>
+  <pds-sortable-item>Item 2</pds-sortable-item>
+  <pds-sortable-item>Item 3</pds-sortable-item>
+</pds-sortable>`;
+
+const HandleTemplate = (args) => html`
+<pds-sortable border="${args.border}" component-id="${args.componentId}" dividers="${args.dividers}" handle-type="${args.handleType}">
+  <pds-sortable-item handle>Item 1</pds-sortable-item>
+  <pds-sortable-item handle>Item 2</pds-sortable-item>
+  <pds-sortable-item handle>Item 3</pds-sortable-item>
+</pds-sortable>`;
+
+const ActionsTemplate = (args) => html`
+<pds-sortable border="${args.border}" component-id="${args.componentId}" dividers="${args.dividers}" handle-type="${args.handleType}">
+  <pds-sortable-item actions>
+    <div>Item 1</div>
+    <div slot="sortable-item-actions">
+      <pds-link href="#" variant="plain">action</pds-link>
+    </div>
+  </pds-sortable-item>
+  <pds-sortable-item actions>
+    <div>Item 2</div>
+    <div slot="sortable-item-actions">
+      <pds-link href="#" variant="plain">action</pds-link>
+    </div>
+  </pds-sortable-item>
+  <pds-sortable-item actions>
+    <div>Item 3</div>
+    <div slot="sortable-item-actions">
+      <pds-link href="#" variant="plain">action</pds-link>
+    </div>
+  </pds-sortable-item>
+</pds-sortable>`;
+
+const FullDemoTemplate = (args) => html`
+<pds-sortable border="${args.border}" component-id="${args.componentId}" dividers="${args.dividers}" handle-type="${args.handleType}">
+  <pds-sortable-item actions handle>
+    <div>
+      <div><strong>Item 1</strong></div>
+      <div>Item 1 description copy text</div>
+    </div>
+    <div slot="sortable-item-actions">
+      <pds-link href="#" variant="plain">action</pds-link>
+    </div>
+  </pds-sortable-item>
+  <pds-sortable-item actions handle>
+    <div>
+      <div><strong>Item 2</strong></div>
+      <div>Item 2 description copy text</div>
+    </div>
+    <div slot="sortable-item-actions">
+      <pds-link href="#" variant="plain">action</pds-link>
+    </div>
+  </pds-sortable-item>
+  <pds-sortable-item actions handle>
+    <div>
+      <div><strong>Item 3</strong></div>
+      <div>Item 3 description copy text</div>
+    </div>
+    <div slot="sortable-item-actions">
+      <pds-link href="#" variant="plain">action</pds-link>
+    </div>
+  </pds-sortable-item>
 </pds-sortable>`;
 
 const sortableEventExample = () => {
@@ -42,12 +100,12 @@ Bordered.args = {
 export const Dividers = BaseTemplate.bind();
 Dividers.args = {
   componentId: 'dividers',
-  border: true,
+  border: false,
   dividers: true,
   handleType: 'row',
 };
 
-export const Handle = BaseTemplate.bind();
+export const Handle = HandleTemplate.bind();
 Handle.args = {
   componentId: 'handle',
   border: false,
@@ -55,7 +113,7 @@ Handle.args = {
   handleType: 'handle',
 };
 
-export const Actions = BaseTemplate.bind();
+export const Actions = ActionsTemplate.bind();
 Actions.args = {
   componentId: 'actions',
   border: false,
@@ -63,10 +121,10 @@ Actions.args = {
   handleType: 'row',
 };
 
-export const FullDemo = BaseTemplate.bind();
+export const FullDemo = FullDemoTemplate.bind();
 FullDemo.args = {
   componentId: 'demo',
-  border: false,
-  dividers: false,
+  border: true,
+  dividers: true,
   handleType: 'row',
 };
