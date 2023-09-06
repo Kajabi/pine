4926b38ee8081b48f7d8bab40de4e0e64f8acb4b
Quinton Jason <quinton.jason@gmail.com>
2023-09-06 16:20:45 -0500

    docs(pds-popover): clean up demo code

--- libs/core/src/components/pds-popover/stories/pds-popover.stories.js
+++ libs/core/src/components/pds-popover/stories/pds-popover.stories.js
@@ -4,6 +4,9 @@ import { withActions } from '@storybook/addon-actions/decorator';
 
 export default {
   argTypes: extractArgTypes('pds-popover'),
+  args: {
+    hasArrow: false
+  },
   component: 'pds-popover',
   decorators: [withActions],
   parameters: {
@@ -15,7 +18,7 @@ export default {
 }
 
 const BaseTemplate = (args) => html`
-  <pds-popover has-arrow=${args.hasArrow} placement=${args.placement} html-content=${args.htmlContent}>
+  <pds-popover has-arrow=${args.hasArrow} placement=${args.placement}>
     <div slot="content">
       <p>Pastrami chuck leberkas, swine biltong tail fatback jowl landjaeger ground round strip steak t-bone cow beef. Filet mignon bresaola pastrami, beef salami hamburger short ribs. Strip steak picanha pork loin tri-tip, prosciutto frankfurter turducken cupim sausage corned beef spare ribs sirloin fatback t-bone bacon. Boudin pork chop capicola short ribs flank rump. Fatback andouille chuck, pork venison t-bone bacon cupim turducken brisket sausage pork loin. Shank buffalo sirloin swine shoulder sausage prosciutto fatback.</p>
       <div>
@@ -50,4 +53,3 @@ WithList.args = {
   hasArrow: false,
   placement: "bottom-start",
 };
-
