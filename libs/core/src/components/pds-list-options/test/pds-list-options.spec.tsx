32f66ebd0b169f687fcfb3dea96d47da2b655edc
Quinton Jason <quinton.jason@gmail.com>
2023-09-11 08:48:50 -0500

    docs(list-options): update stories

--- libs/core/src/components/pds-list-options/test/pds-list-options.spec.tsx
+++ libs/core/src/components/pds-list-options/test/pds-list-options.spec.tsx
@@ -38,4 +38,55 @@ describe('pds-list-options', () => {
       </pds-list-options>
     `);
   });
+
+  it('should handle arrow up key correctly', async () => {
+    const page = await newSpecPage({
+      components: [PdsListOptions],
+      html: `
+      <pds-list-options>
+        <pds-list-option>Item 1</pds-list-option>
+        <pds-list-option>Item 2</pds-list-option>
+      </pds-list-options>
+      `,
+    });
+    const component = page.rootInstance;
+    const mockEvent = new KeyboardEvent('keydown', { key: 'ArrowDown' });
+
+    component.handleKeyDown(mockEvent);
+
+    // Assert that your expectations are met
+    expect(component.focusedOptionIndex).toBe(0); // Assuming you have at least one option
+  });
+
+  it('should handle arrow down key correctly', async () => {
+    const page = await newSpecPage({
+      components: [PdsListOptions],
+      html: `
+      <pds-list-options>
+        <pds-list-option>Item 1</pds-list-option>
+        <pds-list-option>Item 2</pds-list-option>
+      </pds-list-options>
+      `,
+    });
+    const component = page.rootInstance;
+    const mockEventKeyDown = new KeyboardEvent('keydown', { key: 'ArrowDown' });
+    const mockEventKeyUp = new KeyboardEvent('keydown', { key: 'ArrowUp' });
+
+    // simulate keydown
+    component.handleKeyDown(mockEventKeyDown);
+
+    expect(component.focusedOptionIndex).toBe(0);
+
+    // simulate keydown
+    component.handleKeyDown(mockEventKeyDown);
+
+    // Assert that your expectations are met
+    expect(component.focusedOptionIndex).toBe(1);
+
+    // simulate keyup
+    component.handleKeyDown(mockEventKeyUp);
+
+    // Assert that your expectations are met
+    expect(component.focusedOptionIndex).toBe(0);
+  });
 });
