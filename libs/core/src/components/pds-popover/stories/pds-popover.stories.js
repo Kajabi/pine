12a16522eca505a14c13aa2ef4cdd45ee9aeb85e
Quinton Jason <quinton.jason@gmail.com>
2023-09-11 14:41:11 -0500

    docs(popover): add story with avatar dropdown as trigger

--- libs/core/src/components/pds-popover/stories/pds-popover.stories.js
+++ libs/core/src/components/pds-popover/stories/pds-popover.stories.js
@@ -29,8 +29,27 @@ const BaseTemplate = (args) => html`
     <pds-button variant="secondary">Help</pds-button>
   </pds-popover>`;
 
+const AvatarDropdownTemplate = (args) => html`
+  <pds-popover has-arrow=${args.hasArrow} placement=${args.placement}>
+    <div slot="content">
+      <p>Pastrami chuck leberkas, swine biltong tail fatback jowl landjaeger ground round strip steak t-bone cow beef. Filet mignon bresaola pastrami, beef salami hamburger short ribs. Strip steak picanha pork loin tri-tip, prosciutto frankfurter turducken cupim sausage corned beef spare ribs sirloin fatback t-bone bacon. Boudin pork chop capicola short ribs flank rump. Fatback andouille chuck, pork venison t-bone bacon cupim turducken brisket sausage pork loin. Shank buffalo sirloin swine shoulder sausage prosciutto fatback.</p>
+      <div>
+        <pds-button variant="secondary">Cancel</pds-button>
+        <pds-button>Get Started</pds-button>
+      </div>
+    </div>
+    <pds-avatar dropdown="true"></pds-avatar>
+  </pds-popover>`;
+
 export const Default = BaseTemplate.bind({});
 Default.args = {
+  hasArrow: false,
+  htmlContent: true,
+  placement: "bottom-start",
+};
+
+export const AvatarPopover = AvatarDropdownTemplate.bind({});
+AvatarPopover.args = {
   hasArrow: false,
   htmlContent: true,
   placement: "bottom-start",
