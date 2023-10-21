388dd096ac15536e3bccfaadaed37164d749dff4
Phillip Lovelace <phillip@pixelflips.com>
2023-10-20 17:01:21 -0700

    docs(table): add intial stories

--- libs/core/src/components/pds-table/stories/pds-table.stories.js
+++ libs/core/src/components/pds-table/stories/pds-table.stories.js
@@ -1,6 +1,6 @@
 import { html } from 'lit-html';
 import { extractArgTypes } from '@pxtrn/storybook-addon-docs-stencil';
-import { withActions } from '@storybook/addon-actions/decorator';
+// import { withActions } from '@storybook/addon-actions/decorator';
 
 export default {
   argTypes: extractArgTypes('pds-table'),
@@ -15,10 +15,40 @@ export default {
 };
 
 const BaseTemplate = (args) => html`
-<pds-table></pds-table>`;
+<pds-table component-id=${args.componentId} selectable=${args.selectable}>
+  <pds-table-head>
+    <pds-table-head-cell>Column Title</pds-table-head-cell>
+    <pds-table-head-cell>Column Title</pds-table-head-cell>
+    <pds-table-head-cell>Column Title</pds-table-head-cell>
+  </pds-table-head>
+  <pds-table-body>
+    <pds-table-row value="row1">
+      <pds-table-cell>Row Item Alpha</pds-table-cell>
+      <pds-table-cell>Row Item Alpha</pds-table-cell>
+      <pds-table-cell>Row Item Alpha</pds-table-cell>
+    </pds-table-row>
+    <pds-table-row value="row2">
+      <pds-table-cell>Row Item Bravo</pds-table-cell>
+      <pds-table-cell>Row Item Bravo</pds-table-cell>
+      <pds-table-cell>Row Item Bravo</pds-table-cell>
+    </pds-table-row>
+    <pds-table-row value="row3">
+      <pds-table-cell>Row Item Charlie</pds-table-cell>
+      <pds-table-cell>Row Item Charlie</pds-table-cell>
+      <pds-table-cell>Row Item Charlie</pds-table-cell>
+    </pds-table-row>
+  </pds-table-body>
+</pds-table>`;
 
 
 export const Default = BaseTemplate.bind();
 Default.args = {
   componentId: 'default',
+  selectable: false,
+};
+
+export const Selectable = BaseTemplate.bind();
+Selectable.args = {
+  componentId: 'selectable',
+  selectable: true,
 };
