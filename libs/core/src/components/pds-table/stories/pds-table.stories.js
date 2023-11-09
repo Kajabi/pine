095b14641094b389dc073a86693c0370c84412e0
Phillip Lovelace <phillip@pixelflips.com>
2023-11-08 17:05:37 -0800

    docs(table): added initial docs related to sorting

--- libs/core/src/components/pds-table/stories/pds-table.stories.js
+++ libs/core/src/components/pds-table/stories/pds-table.stories.js
@@ -16,10 +16,11 @@ export default {
 
 const BaseTemplate = (args) => html`
 <pds-table
-  compact=${args.compact}
-  component-id=${args.componentId}
-  responsive=${args.responsive}
-  selectable=${args.selectable}
+  compact="${args.compact}"
+  component-id="${args.componentId}"
+  fixed-column="${args.fixedColumn}"
+  responsive="${args.responsive}"
+  selectable="${args.selectable}"
 >
   <pds-table-head>
     <pds-table-head-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">Column Title</pds-table-head-cell>
@@ -47,11 +48,11 @@ const BaseTemplate = (args) => html`
 
 const ResponsiveTemplate = (args) => html`
 <pds-table
-  compact=${args.compact}
-  component-id=${args.componentId}
+  compact="${args.compact}"
+  component-id="${args.componentId}"
   fixed-column="${args.fixedColumn}"
-  responsive=${args.responsive}
-  selectable=${args.selectable}
+  responsive="${args.responsive}"
+  selectable="${args.selectable}"
 >
   <pds-table-head>
     <pds-table-head-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">Column Title</pds-table-head-cell>
@@ -113,6 +114,63 @@ const ResponsiveTemplate = (args) => html`
   </pds-table-body>
 </pds-table>`;
 
+const SortableTemplate = (args) => html`
+<pds-table
+  compact="${args.compact}"
+  component-id="${args.componentId}"
+  fixed-column="${args.fixedColumn}"
+  responsive="${args.responsive}"
+  selectable="${args.selectable}"
+>
+  <pds-table-head>
+    <pds-table-head-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}" sortable=${args.sortable}>Name</pds-table-head-cell>
+    <pds-table-head-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}" sortable=${args.sortable}>Email</pds-table-head-cell>
+    <pds-table-head-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}" sortable=${args.sortable}>Email Marketing</pds-table-head-cell>
+  </pds-table-head>
+  <pds-table-body>
+    <pds-table-row>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">John Doe</pds-table-cell>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">john.doe@example.com</pds-table-cell>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">Subscribed</pds-table-cell>
+    </pds-table-row>
+    <pds-table-row>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">Jane Smith</pds-table-cell>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">jane.smith@example.com</pds-table-cell>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">Not Subscribed</pds-table-cell>
+    </pds-table-row>
+    <pds-table-row>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">Michael Johnson</pds-table-cell>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">michael.johnson@example.com</pds-table-cell>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">Subscribed</pds-table-cell>
+    </pds-table-row>
+    <pds-table-row>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">Susan Brown</pds-table-cell>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">susan.brown@example.com</pds-table-cell>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">Not Subscribed</pds-table-cell>
+    </pds-table-row>
+    <pds-table-row>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">William Davis</pds-table-cell>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">william.davis@example.com</pds-table-cell>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">Subscribed</pds-table-cell>
+    </pds-table-row>
+    <pds-table-row>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">Mary Wilson</pds-table-cell>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">mary.wilson@example.com</pds-table-cell>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">Not Subscribed</pds-table-cell>
+    </pds-table-row>
+    <pds-table-row>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">James Lee</pds-table-cell>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">james.lee@example.com</pds-table-cell>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">Subscribed</pds-table-cell>
+    </pds-table-row>
+    <pds-table-row>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">Linda Hall</pds-table-cell>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">linda.hall@example.com</pds-table-cell>
+      <pds-table-cell class="${args.compact ? 'hydrated is-compact' : 'hydrated'}">Not Subscribed</pds-table-cell>
+    </pds-table-row>
+  </pds-table-body>
+</pds-table>`;
+
 export const Default = BaseTemplate.bind();
 Default.args = {
   compact: false,
@@ -156,4 +214,14 @@ fixedColumn.args = {
   fixedColumn: true,
   responsive: true,
   selectable: false,
+};
+
+export const Sortable = SortableTemplate.bind();
+Sortable.args = {
+  compact: false,
+  componentId: 'sortable',
+  fixedColumn: false,
+  responsive: false,
+  selectable: false,
+  sortable: true,
 };
