059e08984e6b8f36b58505e2b32bfff96ebd6d6a
Phillip Lovelace <phillip@pixelflips.com>
2023-11-13 13:22:53 -0800

    test(table): add spec test for cell

--- libs/core/src/components/pds-table/pds-table-cell/test/pds-table-cell.spec.tsx
+++ libs/core/src/components/pds-table/pds-table-cell/test/pds-table-cell.spec.tsx
@@ -1,5 +1,6 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { PdsTableCell } from '../pds-table-cell';
+import { PdsTable } from '../../pds-table';
 
 describe('pds-table-cell', () => {
   it('renders', async () => {
@@ -7,6 +8,7 @@ describe('pds-table-cell', () => {
       components: [PdsTableCell],
       html: `<pds-table-cell></pds-table-cell>`,
     });
+
     expect(page.root).toEqualHtml(`
       <pds-table-cell role="gridcell">
         <mock:shadow-root>
@@ -14,5 +16,40 @@ describe('pds-table-cell', () => {
         </mock:shadow-root>
       </pds-table-cell>
     `);
+  });
+
+  it('renders truncated when prop is set', async () => {
+    const page = await newSpecPage({
+      components: [PdsTableCell],
+      html: `<pds-table-cell truncate="true"></pds-table>`,
+    });
+
+    expect(page.root).toEqualHtml(`
+      <pds-table-cell role="gridcell" truncate="true" class="is-truncated">
+        <mock:shadow-root>
+          <slot></slot>
+        </mock:shadow-root>
+      </pds-table-cell>
+    `);
+  });
+
+  it('renders with is-compact class when tableRef is compact', async () => {
+    const page = await newSpecPage({
+      components: [PdsTableCell, PdsTable],
+      html: `<pds-table compact><pds-table-cell></pds-table-cell></pds-table>`,
+    });
+
+    const tableCell = page.body.querySelector('pds-table-cell') as any;
+    expect(tableCell.classList.contains('is-compact')).toBeTruthy();
+  });
+
+  it('renders with fixed-cell-position style when tableRef is fixed & selectable', async () => {
+    const page = await newSpecPage({
+      components: [PdsTableCell, PdsTable],
+      html: `<pds-table fixed-column selectable><pds-table-cell></pds-table-cell></pds-table>`,
+    });
+
+    const tableCell = page.body.querySelector('pds-table-cell') as any;
+    expect(tableCell.style.getPropertyValue('--fixed-cell-position')).toBe('40px');
   });
 });
