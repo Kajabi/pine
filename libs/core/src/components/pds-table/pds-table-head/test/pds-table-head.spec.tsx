3caefe65ba2d1d7c220343ec20ed3d00ed395eac
Phillip Lovelace <phillip@pixelflips.com>
2023-11-13 14:23:53 -0800

    test(table): add spec tests for head

--- libs/core/src/components/pds-table/pds-table-head/test/pds-table-head.spec.tsx
+++ libs/core/src/components/pds-table/pds-table-head/test/pds-table-head.spec.tsx
@@ -1,5 +1,6 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { PdsTableHead } from '../pds-table-head';
+import { PdsTable } from '../../pds-table';
 
 describe('pds-table-head', () => {
   it('renders', async () => {
@@ -15,4 +16,64 @@ describe('pds-table-head', () => {
       </pds-table-head>
     `);
   });
+
+  it('renders with is-fixed class when tableRef has fixedColumn', async () => {
+    const page = await newSpecPage({
+      components: [PdsTableHead, PdsTable],
+      html: `
+        <pds-table fixed-column="true">
+          <pds-table-head>
+            <pds-table-head-cell>Column Title</pds-table-head-cell>
+          </pds-table-head>
+        </pds-table>`,
+    });
+
+    const tableHeadCell = page.body.querySelector('pds-table-head-cell');
+    expect(tableHeadCell?.classList.contains('is-fixed')).toBeTruthy();
+  });
+
+  it('renders pds-table-checkbox-cell with checkbox-cell part', async () => {
+    const page = await newSpecPage({
+      components: [PdsTableHead, PdsTable],
+      html: `
+        <pds-table selectable="true" fixed-column="true">
+          <pds-table-head></pds-table-head>
+        </pds-table>`,
+    });
+
+    const checkboxCell = page.body.querySelector('pds-table-checkbox-cell');
+    const tableHead = page.body.querySelector('pds-table-head');
+
+    expect(checkboxCell).toBeDefined();
+    expect(tableHead).toBeDefined();
+
+    if (checkboxCell && tableHead) {
+      await page.waitForChanges();
+
+      expect(checkboxCell.getAttribute('part')).toBe('checkbox-cell');
+    }
+  });
+
+  it('renders pds-table-checkbox-cell without checkbox-cell part', async () => {
+    const page = await newSpecPage({
+      components: [PdsTableHead, PdsTable],
+      html: `
+        <pds-table selectable="true" fixed-column="false">
+          <pds-table-head></pds-table-head>
+        </pds-table>`,
+    });
+
+    const checkboxCell = page.body.querySelector('pds-table-checkbox-cell');
+    const tableHead = page.body.querySelector('pds-table-head');
+
+    expect(checkboxCell).toBeDefined();
+    expect(tableHead).toBeDefined();
+
+    if (checkboxCell && tableHead) {
+      await page.waitForChanges();
+
+      expect(checkboxCell.getAttribute('part')).toBe('');
+    }
+  });
+
 });
