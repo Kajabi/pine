78e408b92bfc192dd24ad1e57557f2d44206714b
Phillip Lovelace <phillip@pixelflips.com>
2023-11-14 10:20:39 -0800

    test(table): add spec tests for table

--- libs/core/src/components/pds-table/test/pds-table.spec.tsx
+++ libs/core/src/components/pds-table/test/pds-table.spec.tsx
@@ -15,4 +15,36 @@ describe('pds-table', () => {
       </pds-table>
     `);
   });
+
+  it('renders compact when prop is set', async () => {
+    const page = await newSpecPage({
+      components: [PdsTable],
+      html: `<pds-table compact="true"></pds-table>`,
+    });
+
+    expect(page.root).toEqualHtml(`
+      <pds-table class="pds-table is-compact" compact="true" role="grid">
+        <mock:shadow-root>
+          <slot></slot>
+        </mock:shadow-root>
+      </pds-table>
+    `);
+  });
+
+  it('renders responsive when prop is set', async () => {
+    const page = await newSpecPage({
+      components: [PdsTable],
+      html: `<pds-table responsive="true"></pds-table>`,
+    });
+
+    expect(page.root).toEqualHtml(`
+      <pds-table class="pds-table is-responsive" responsive="true" role="grid">
+        <mock:shadow-root>
+          <slot></slot>
+        </mock:shadow-root>
+      </pds-table>
+    `);
+  });
+
+
 });
