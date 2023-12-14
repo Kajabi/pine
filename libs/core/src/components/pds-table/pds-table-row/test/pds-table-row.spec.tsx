27bd1c5f6b84d59e31431c76ced5627dba3854d6
Phillip Lovelace <phillip@pixelflips.com>
2023-12-14 14:52:07 -0800

    test(table): add additional test for table-row

--- libs/core/src/components/pds-table/pds-table-row/test/pds-table-row.spec.tsx
+++ libs/core/src/components/pds-table/pds-table-row/test/pds-table-row.spec.tsx
@@ -1,4 +1,6 @@
 import { newSpecPage } from '@stencil/core/testing';
+import { PdsTable } from '../../pds-table';
+import { PdsTableBody } from '../../pds-table-body/pds-table-body';
 import { PdsTableRow } from '../pds-table-row';
 
 describe('pds-table-row', () => {
@@ -15,4 +17,26 @@ describe('pds-table-row', () => {
       </pds-table-row>
     `);
   });
+
+  it('renders with is-selected class when the checkbox is checked', async () => {
+    const page = await newSpecPage({
+      components: [PdsTable, PdsTableBody, PdsTableRow],
+      html: `
+      <pds-table selectable="true">
+        <pds-table-body>
+          <pds-table-row></pds-table-row>
+        </pds-table-body>
+      </pds-table>
+      `
+    });
+
+    const row = page.root?.querySelector('pds-table-row');
+    const checkbox = row?.shadowRoot?.querySelector('pds-checkbox') as HTMLFormElement;
+
+    checkbox.click();
+    await page.waitForChanges();
+
+    expect(row?.classList.contains('is-selected')).toBe(true);
+  });
+
 });
