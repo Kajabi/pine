e5005520663bebd43da3884912034da263a63983
Phillip Lovelace <phillip@pixelflips.com>
2023-12-18 10:16:40 -0800

    test(table): add additional e2e tests for table row

--- libs/core/src/components/pds-table/pds-table-row/test/pds-table-row.e2e.ts
+++ libs/core/src/components/pds-table/pds-table-row/test/pds-table-row.e2e.ts
@@ -3,9 +3,50 @@ import { newE2EPage } from '@stencil/core/testing';
 describe('pds-table-row', () => {
   it('renders', async () => {
     const page = await newE2EPage();
-    await page.setContent('<pds-table-row></pds-table-row>');
+    await page.setContent(`<pds-table-row></pds-table-row>`);
+    const row = await page.find('pds-table-row');
+    expect(row).not.toBeNull();
+  });
 
-    const element = await page.find('pds-table-row');
-    expect(element).toHaveClass('hydrated');
+  it('renders with is-selected class when the checkbox is checked', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+      <pds-table selectable>
+        <pds-table-body>
+          <pds-table-row></pds-table-row>
+        </pds-table-body>
+      </pds-table>
+    `);
+
+    const row = await page.find('pds-table-row');
+    const checkbox = await page.find('pds-table-row >>> pds-checkbox');
+
+    await checkbox.click();
+    await page.waitForChanges();
+
+    const classList = await row.getAttribute('class');
+    expect(classList).toContain('is-selected');
+  });
+
+
+  it('emits pdsTableRowSelected event when the checkbox is clicked', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+      <pds-table selectable="true">
+        <pds-table-body>
+          <pds-table-row></pds-table-row>
+        </pds-table-body>
+      </pds-table>
+    `);
+
+    const row = await page.find('pds-table-row');
+    const checkbox = await page.find('pds-table-row >>> pds-checkbox');
+
+    const pdsTableRowSelectedSpy = await row.spyOnEvent('pdsTableRowSelected');
+
+    await checkbox.click();
+    await page.waitForChanges();
+
+    expect(pdsTableRowSelectedSpy).toHaveReceivedEventDetail({ rowIndex: 0, isSelected: true });
   });
 });
