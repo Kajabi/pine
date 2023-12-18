edbdf9fc33d18a8e7fa16c7592d2d3856194a1dc
Phillip Lovelace <phillip@pixelflips.com>
2023-12-18 10:50:50 -0800

    test(table): add additional e2e tests for table cell

--- libs/core/src/components/pds-table/pds-table-cell/test/pds-table-cell.e2e.ts
+++ libs/core/src/components/pds-table/pds-table-cell/test/pds-table-cell.e2e.ts
@@ -8,4 +8,37 @@ describe('pds-table-cell', () => {
     const element = await page.find('pds-table-cell');
     expect(element).toHaveClass('hydrated');
   });
+
+  it('renders with compact styles', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+      <pds-table compact>
+        <pds-table-body>
+          <pds-table-row>
+            <pds-table-cell>Row 1, Column 1</pds-table-cell>
+            <pds-table-cell>Row 1, Column 2</pds-table-cell>
+          </pds-table-row>
+        </pds-table-body>
+      </pds-table>
+    `);
+
+    const tableCell = await page.find('pds-table-cell');
+    expect(tableCell).toHaveClass('is-compact');
+  });
+
+  it('renders with truncated content', async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+      <pds-table>
+        <pds-table-body>
+          <pds-table-row>
+            <pds-table-cell truncate>Very long content that should be truncated</pds-table-cell>
+          </pds-table-row>
+        </pds-table-body>
+      </pds-table>
+    `);
+
+    const tableCell = await page.find('pds-table-cell');
+    expect(tableCell).toHaveClass('is-truncated');
+  });
 });
