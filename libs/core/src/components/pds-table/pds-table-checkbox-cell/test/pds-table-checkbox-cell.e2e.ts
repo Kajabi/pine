c7901589bd4d4f399d49d213dc23840f011cf0a2
Phillip Lovelace <phillip@pixelflips.com>
2023-12-18 10:51:11 -0800

    test(table): add additional e2e tests for table checkbox cell

--- libs/core/src/components/pds-table/pds-table-checkbox-cell/test/pds-table-checkbox-cell.e2e.ts
+++ libs/core/src/components/pds-table/pds-table-checkbox-cell/test/pds-table-checkbox-cell.e2e.ts
@@ -3,9 +3,23 @@ import { newE2EPage } from '@stencil/core/testing';
 describe('pds-table-checkbox-cell', () => {
   it('renders', async () => {
     const page = await newE2EPage();
-    await page.setContent('<pds-table-checkbox-cell></pds-table-checkbox-cell>');
+    await page.setContent(`
+      <pds-table>
+        <pds-table-body>
+          <pds-table-row>
+            <pds-table-checkbox-cell>
+              <pds-checkbox componentId="checkbox-1" label="Select Row" labelHidden="true"></pds-checkbox>
+            </pds-table-checkbox-cell>
+          </pds-table-row>
+        </pds-table-body>
+      </pds-table>
+    `);
 
-    const element = await page.find('pds-table-checkbox-cell');
-    expect(element).toHaveClass('hydrated');
+    const checkboxCell = await page.find('pds-table-checkbox-cell');
+    expect(checkboxCell).not.toBeNull();
+
+    const checkbox = await page.find('pds-checkbox');
+    expect(checkbox).not.toBeNull();
   });
+
 });
