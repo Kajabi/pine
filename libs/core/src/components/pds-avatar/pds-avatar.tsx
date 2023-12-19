11de82467957e1bd4b88461084ac624deed5015c
Ashley Echols <ashley.echols@kajabi.com>
2023-12-19 11:13:27 -0600

    Revert "refactor: removes has-image CSS class to use has selector"
    This reverts commit 64d1942583bdcdea6eb362ffc9b2a279a2cfc281.

--- libs/core/src/components/pds-avatar/pds-avatar.tsx
+++ libs/core/src/components/pds-avatar/pds-avatar.tsx
@@ -112,6 +112,7 @@ export class PdsAvatar {
   private classNames = () => (
     {
       'pds-avatar': true,
+      [`pds-avatar--has-image`]: this.image !== '' && this.image !== null, // Remove when FF supports :has selector
       [`pds-avatar--${this.variant}`]: this.variant === 'admin'
     }
   );
