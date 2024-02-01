c2ee64cd8d691e8fa6caa9356a3f1c3a238ec682
Julian Skinner <ju-Skinner@users.noreply.github.com>
2024-02-01 13:58:09 -0600

    fix(checkbox): event details (#137)
    * fix(checkbox): event details added  to the pdsCheckboxChange event
    
    * docs(checkbox): adds a new section for Events

--- libs/react/src/components/proxies.ts
+++ libs/react/src/components/proxies.ts
@@ -5,6 +5,7 @@ import { createReactComponent } from './react-component-lib';
 
 import type { JSX } from '@pine-ds/core/components';
 
+import { defineCustomElement as definePdsAccordion } from '@pine-ds/core/components/pds-accordion.js';
 import { defineCustomElement as definePdsAvatar } from '@pine-ds/core/components/pds-avatar.js';
 import { defineCustomElement as definePdsButton } from '@pine-ds/core/components/pds-button.js';
 import { defineCustomElement as definePdsCheckbox } from '@pine-ds/core/components/pds-checkbox.js';
@@ -31,6 +32,7 @@ import { defineCustomElement as definePdsTabs } from '@pine-ds/core/components/p
 import { defineCustomElement as definePdsTextarea } from '@pine-ds/core/components/pds-textarea.js';
 import { defineCustomElement as definePdsTooltip } from '@pine-ds/core/components/pds-tooltip.js';
 
+export const PdsAccordion = /*@__PURE__*/createReactComponent<JSX.PdsAccordion, HTMLPdsAccordionElement>('pds-accordion', undefined, undefined, definePdsAccordion);
 export const PdsAvatar = /*@__PURE__*/createReactComponent<JSX.PdsAvatar, HTMLPdsAvatarElement>('pds-avatar', undefined, undefined, definePdsAvatar);
 export const PdsButton = /*@__PURE__*/createReactComponent<JSX.PdsButton, HTMLPdsButtonElement>('pds-button', undefined, undefined, definePdsButton);
 export const PdsCheckbox = /*@__PURE__*/createReactComponent<JSX.PdsCheckbox, HTMLPdsCheckboxElement>('pds-checkbox', undefined, undefined, definePdsCheckbox);
