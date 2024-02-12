591b717e2ba91c721812fac3c8854c72f4a7d4fb
Quinton Jason <quinton.jason@gmail.com>
2024-02-12 10:56:58 -0600

    docs(pds-box): add props to playground story

--- libs/core/src/components/pds-box/stories/pds-box.stories.js
+++ libs/core/src/components/pds-box/stories/pds-box.stories.js
@@ -13,17 +13,31 @@ export default {
 const BaseTemplate = (args) => html`
 <pds-box
 	align-items="${args.alignItems}"
+  auto="${args.auto}"
   background-color="${args.backgroundColor}"
 	border="${args.border}"
   border-color="${args.borderColor}"
 	border-radius="${args.borderRadius}"
   direction="${args.direction}"
 	display="${args.display}"
+  fit="${args.fit}"
 	justify-content="${args.justifyContent}"
   min-height="${args.minHeight}"
   min-width="${args.minWidth}"
+  offset="${args.offset}"
+  offset-xs="${args.offsetXs}"
+  offset-sm="${args.offsetSm}"
+  offset-md="${args.offsetMd}"
+  offset-lg="${args.offsetLg}"
+  offset-xl="${args.offsetXl}"
   padding="${args.padding}"
   shadow="${args.shadow}"
+  size="${args.size}"
+  size-xs="${args.sizeXs}"
+  size-sm="${args.sizeSm}"
+  size-md="${args.sizeMd}"
+  size-lg="${args.sizeLg}"
+  size-xl="${args.sizeXl}"
 >
   Box content
 </pds-box>`;
