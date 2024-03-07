d61dfb9ae78b04714034fc29f847960c88e7aada
Quinton Jason <quinton.jason@gmail.com>
2024-03-07 17:36:08 -0600

    test(pds-avatar): add part to test and resolve dropdown props exception

--- libs/core/src/components/pds-avatar/test/pds-avatar.spec.tsx
+++ libs/core/src/components/pds-avatar/test/pds-avatar.spec.tsx
@@ -10,7 +10,7 @@ describe('pds-avatar', () => {
     expect(page.root).toEqualHtml(`
       <pds-avatar class="pds-avatar" size="lg" variant="customer">
         <mock:shadow-root>
-          <div style="height: 56px; width: 56px;">
+          <div part="asset-wrapper" style="height: 56px; width: 56px;">
             <pds-icon name="user-filled" size="33.53%"></pds-icon>
           </div>
         </mock:shadow-root>
@@ -27,7 +27,7 @@ describe('pds-avatar', () => {
     expect(page.root).toEqualHtml(`
       <pds-avatar component-id="test" id="test" class="pds-avatar" size="lg" variant="customer">
         <mock:shadow-root>
-          <div style="height: 56px; width: 56px;">
+          <div part="asset-wrapper" style="height: 56px; width: 56px;">
             <pds-icon name="user-filled" size="33.53%"></pds-icon>
           </div>
         </mock:shadow-root>
@@ -43,7 +43,7 @@ describe('pds-avatar', () => {
     expect(page.root).toEqualHtml(`
       <pds-avatar class="pds-avatar pds-avatar--admin" size="lg" variant="admin">
         <mock:shadow-root>
-          <div style="height: 56px; width: 56px">
+          <div part="asset-wrapper" style="height: 56px; width: 56px">
             <pds-icon name="user-filled" size="33.53%"></pds-icon>
           </div>
         </mock:shadow-root>
@@ -59,7 +59,7 @@ describe('pds-avatar', () => {
     expect(page.root).toEqualHtml(`
       <pds-avatar class="pds-avatar" badge="true" size="lg" variant="customer">
         <mock:shadow-root>
-          <div style="height: 56px; width: 56px;">
+          <div part="asset-wrapper" style="height: 56px; width: 56px;">
             <pds-icon name="user-filled" size="33.53%"></pds-icon>
             <pds-icon class="pds-avatar__badge" name="check-circle-filled" size="33.53%"></pds-icon>
           </div>
@@ -76,7 +76,7 @@ describe('pds-avatar', () => {
     expect(page.root).toEqualHtml(`
       <pds-avatar class="pds-avatar" size="128px" variant="customer">
         <mock:shadow-root>
-          <div style="height: 128px; width: 128px">
+          <div part="asset-wrapper" style="height: 128px; width: 128px">
             <pds-icon name="user-filled" size="33.53%"></pds-icon>
           </div>
         </mock:shadow-root>
@@ -92,7 +92,7 @@ describe('pds-avatar', () => {
     expect(page.root).toEqualHtml(`
       <pds-avatar class="pds-avatar pds-avatar--has-image" image="https://placehold.co/64x64" size="lg" variant="customer">
         <mock:shadow-root>
-          <div style="height: 56px; width: 56px;">
+          <div part="asset-wrapper" style="height: 56px; width: 56px;">
             <img src="https://placehold.co/64x64"/>
           </div>
         </mock:shadow-root>
@@ -108,7 +108,7 @@ describe('pds-avatar', () => {
     expect(page.root).toEqualHtml(`
       <pds-avatar class="pds-avatar pds-avatar--has-image" alt="Customer Profile" image="https://placehold.co/64x64" size="lg" variant="customer">
         <mock:shadow-root>
-          <div style="height: 56px; width: 56px;">
+          <div part="asset-wrapper" style="height: 56px; width: 56px;">
             <img alt="Customer Profile" src="https://placehold.co/64x64"/>
           </div>
         </mock:shadow-root>
@@ -124,7 +124,7 @@ describe('pds-avatar', () => {
     expect(page.root).toEqualHtml(`
       <pds-avatar class="pds-avatar pds-avatar--has-image" badge="true" alt="Customer Profile" image="https://placehold.co/64x64" size="lg" variant="customer">
         <mock:shadow-root>
-          <div style="height: 56px; width: 56px">
+          <div part="asset-wrapper" style="height: 56px; width: 56px">
             <img alt="Customer Profile" src="https://placehold.co/64x64"/>
             <pds-icon class="pds-avatar__badge" name="check-circle-filled" size="33.53%"></pds-icon>
           </div>
@@ -141,11 +141,46 @@ describe('pds-avatar', () => {
     expect(page.root).toEqualHtml(`
       <pds-avatar class="pds-avatar" size="xl" variant="customer">
         <mock:shadow-root>
-          <div style="height: 64px; width: 64px;">
+          <div part="asset-wrapper" style="height: 64px; width: 64px;">
             <pds-icon name="user-filled" size="33.53%"></pds-icon>
           </div>
         </mock:shadow-root>
       </pds-avatar>
     `);
   });
+
+  it('renders avatar as a button when dropdown prop is true', async () => {
+    const page = await newSpecPage({
+      components: [PdsAvatar],
+      html: `<pds-avatar dropdown="true"></pds-avatar>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <pds-avatar class="pds-avatar" dropdown="true" size="lg" variant="customer">
+        <mock:shadow-root>
+          <button class="pds-avatar__button" type="button">
+            <div part="asset-wrapper" style="height: 56px; width: 56px;">
+              <pds-icon name="user-filled" size="33.53%"></pds-icon>
+            </div>
+          </button>
+        </mock:shadow-root>
+      </pds-avatar>
+    `);
+  });
+  
+  it('renders avatar without button when dropdown prop is false', async () => {
+    const page = await newSpecPage({
+      components: [PdsAvatar],
+      html: `<pds-avatar dropdown="false"></pds-avatar>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <pds-avatar class="pds-avatar" dropdown="false" size="lg" variant="customer">
+        <mock:shadow-root>
+          <div part="asset-wrapper" style="height: 56px; width: 56px;">
+            <pds-icon name="user-filled" size="33.53%"></pds-icon>
+          </div>
+        </mock:shadow-root>
+      </pds-avatar>
+    `);
+  });
+  
 });
