f71bc35853df2aec4cc289f747273648e5659faf
Ashley Echols <ashley.echols@kajabi.com>
2024-03-13 13:43:51 -0500

    fix(accordion): uses icon import in tests

--- libs/core/src/components/pds-accordion/test/pds-accordion.spec.tsx
+++ libs/core/src/components/pds-accordion/test/pds-accordion.spec.tsx
@@ -1,5 +1,6 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { PdsAccordion } from '../pds-accordion';
+import { downSmall, upSmall } from '@pine-ds/icons/icons';
 
 describe('pds-accordion', () => {
   it('renders the accordion', async () => {
@@ -13,7 +14,7 @@ describe('pds-accordion', () => {
             <details>
               <summary>
                 <slot name="label">Details</slot>
-                <pds-icon icon="data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' class='pdsicon'><path fill-rule='evenodd' d='M3.594 5.594a.889.889 0 0 1 1.257 0L7.778 8.52l2.927-2.927a.889.889 0 0 1 1.257 1.257l-3.556 3.555a.889.889 0 0 1-1.257 0L3.594 6.851a.889.889 0 0 1 0-1.257Z'/></svg>"></pds-icon>
+                <pds-icon icon="${downSmall}"></pds-icon>
               </summary>
               <div class="pds-accordion__body">
                 <slot />
@@ -35,7 +36,7 @@ describe('pds-accordion', () => {
             <details open>
               <summary>
                 <slot name="label">Details</slot>
-                <pds-icon icon="data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' class='pdsicon'><path fill-rule='evenodd' d='M7.15 5.594a.889.889 0 0 1 1.256 0l3.556 3.555a.889.889 0 0 1-1.257 1.257L7.778 7.48 4.85 10.406A.889.889 0 1 1 3.594 9.15l3.555-3.555Z'/></svg>"></pds-icon>
+                <pds-icon icon="${upSmall}"></pds-icon>
               </summary>
               <div class="pds-accordion__body">
                 <slot />
@@ -58,7 +59,7 @@ describe('pds-accordion', () => {
           <details>
             <summary>
               <slot name="label">Details</slot>
-              <pds-icon icon="data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' class='pdsicon'><path fill-rule='evenodd' d='M3.594 5.594a.889.889 0 0 1 1.257 0L7.778 8.52l2.927-2.927a.889.889 0 0 1 1.257 1.257l-3.556 3.555a.889.889 0 0 1-1.257 0L3.594 6.851a.889.889 0 0 1 0-1.257Z'/></svg>"></pds-icon>
+              <pds-icon icon="${downSmall}"></pds-icon>
             </summary>
             <div class="pds-accordion__body">
               <slot />
@@ -81,7 +82,7 @@ describe('pds-accordion', () => {
           <details>
             <summary>
               <slot name="label">Details</slot>
-              <pds-icon icon="data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' class='pdsicon'><path fill-rule='evenodd' d='M3.594 5.594a.889.889 0 0 1 1.257 0L7.778 8.52l2.927-2.927a.889.889 0 0 1 1.257 1.257l-3.556 3.555a.889.889 0 0 1-1.257 0L3.594 6.851a.889.889 0 0 1 0-1.257Z'/></svg>"></pds-icon>
+              <pds-icon icon="${downSmall}"></pds-icon>
             </summary>
             <div class="pds-accordion__body">
               <slot />
@@ -105,7 +106,7 @@ describe('pds-accordion', () => {
           <details>
             <summary>
               <slot name="label">Details</slot>
-              <pds-icon icon="data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' class='pdsicon'><path fill-rule='evenodd' d='M3.594 5.594a.889.889 0 0 1 1.257 0L7.778 8.52l2.927-2.927a.889.889 0 0 1 1.257 1.257l-3.556 3.555a.889.889 0 0 1-1.257 0L3.594 6.851a.889.889 0 0 1 0-1.257Z'/></svg>"></pds-icon>
+              <pds-icon icon="${downSmall}"></pds-icon>
             </summary>
             <div class="pds-accordion__body">
               <slot />
@@ -129,7 +130,7 @@ describe('pds-accordion', () => {
           <details open>
             <summary>
               <slot name="label">Details</slot>
-              <pds-icon icon="data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' class='pdsicon'><path fill-rule='evenodd' d='M7.15 5.594a.889.889 0 0 1 1.256 0l3.556 3.555a.889.889 0 0 1-1.257 1.257L7.778 7.48 4.85 10.406A.889.889 0 1 1 3.594 9.15l3.555-3.555Z'/></svg>"></pds-icon>
+              <pds-icon icon="${upSmall}"></pds-icon>
             </summary>
             <div class="pds-accordion__body">
               <slot />
