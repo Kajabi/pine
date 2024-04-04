c2f8fb0586484071a74da13c27b48ec2ffeb9dfb
Ashley Echols <ashley.echols@kajabi.com>
2024-04-04 16:42:19 -0500

    docs: creates script to transform tokens exported from tokens studio

---
+++ libs/core/scripts/sd-transforms.mjs
@@ -0,0 +1,22 @@
+import { registerTransforms } from '@tokens-studio/sd-transforms';
+import StyleDictionary from 'style-dictionary';
+// will register them on StyleDictionary object
+// that is installed as a dependency of this package.
+registerTransforms(StyleDictionary);
+const sd = new StyleDictionary({
+    source: ["src/global/styles/tokens/core/*.json"],
+    platforms: {
+        css: {
+            transformGroup: "tokens-studio",
+            transforms: ["name/kebab"],
+            buildPath: "src/global/styles/tokens/semantic/",
+            files: [{
+                    "destination": "_tokens.scss",
+                    "format": "css/variables"
+                }],
+            prefix: "pine"
+        }
+    }
+});
+await sd.cleanAllPlatforms();
+await sd.buildAllPlatforms();
