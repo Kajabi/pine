a97417afd66eb1ab447119c507ca77617d3cddf7
Quinton Jason <quinton.jason@gmail.com>
2024-04-24 09:45:30 -0500

    fix(token-table): updates types to resolve implicit any errors

--- libs/doc-components/src/components/docTokenTable/docTokenTable.tsx
+++ libs/doc-components/src/components/docTokenTable/docTokenTable.tsx
@@ -10,6 +10,7 @@ interface Token {
 }
 
 interface DocTokenTableProps {
+  type: string;
   category: string;
 }
 
@@ -28,110 +29,108 @@ const findValueByKey = (obj: any, keyPath: string): string | undefined => {
     value = value[key];
   }
 
-  console.log('keypath: ', keyPath);
-
   return 'value' in value ? value.value : undefined;
 };
 
 const DocTokenTable: React.FC<DocTokenTableProps> = ({ type, category }) => {
-  console.log('all core tokens: ', allTokenJson);
+  let pineTokens: Token | TokenEntry = {};
   
-  let pineTokens: Token = {};
-
   if (type == 'core') {
-    pineTokens = allTokenJson.core[category];
+    pineTokens = allTokenJson.core[category as keyof typeof allTokenJson.core] as Token;
   } else if (type == 'semantic') {
-    pineTokens = allTokenJson.semantic[category];
+    pineTokens = allTokenJson.semantic[category as keyof typeof allTokenJson.semantic] as Token || {};
   }
   
-  const renderTableRows = (tokens: Token, parentKey?: string) => {
-    return Object.entries(tokens).map(([key, token]) => {
+  const renderTableRows = (tokens: Token, parentKey?: string): JSX.Element[] => {
+    return Object.entries(tokens).map(([key, token]): JSX.Element => {
       const fullKey = parentKey ? `${parentKey}-${key}` : key;
       const prefixedKey = `--pine-${category}-${fullKey}`;
       let style: React.CSSProperties = {};
   
       if ('value' in token) {
-        let matchingValue;
+        let matchingValue: string | undefined;
+
         if (typeof token.value === 'object') {
-          const keys = Object.keys(token.value);
-          const lastKey = keys[keys.length - 1];
-          matchingValue = token.value[lastKey];
+          if ('value' in token.value) {
+            matchingValue = token.value.value as string;
+          }
         } else {
           matchingValue = token.value.startsWith('{') && token.value.endsWith('}')
             ? findValueByKey(allTokenJson.core, token.value.slice(1, -1))
             : token.value;
         }
-  
-        switch (category) {
-          case 'color':
-            if (matchingValue.startsWith('#')) {
-              style.backgroundColor = matchingValue;
+        
+        if (matchingValue) {
+          switch (category) {
+            case 'color':
+              if (matchingValue.startsWith('#')) {
+                style.backgroundColor = matchingValue;
+                style.width = "30px";
+                style.height = "30px";
+              } else {
+                style.color = matchingValue;
+              }
+              break;
+            case 'border':
+              style.border = matchingValue;
               style.width = "30px";
               style.height = "30px";
-            } else {
-              style.color = matchingValue;
-            }
-            break;
-          case 'border':
-            style.border = matchingValue;
-            style.width = "30px";
-            style.height = "30px";
-            break;
-          case 'border-radius':
-            style.borderRadius = matchingValue;
-            style.width = "100px";
-            style.height = "50px";
-            style.border = "1px solid black";
-            break;
-          case 'box-shadow':
-            style.boxShadow = matchingValue;
-            break;
-          case 'letter-spacing':
-            style.letterSpacing = matchingValue;
-            break;
-          case 'line-height':
-            style.lineHeight = matchingValue;
-            break;
-          case 'font-weight':
-            style.fontWeight = matchingValue;
-            break;
-          case 'font-family':
-            style.fontFamily = matchingValue;
-            break;
-          case 'font-size':
-            style.fontSize = matchingValue;
-            break;
-          case 'spacing':
-            // Render spacing cell
-            return (
-              <tr key={fullKey}>
-                <td>
-                  <div style={{ display: 'flex', alignItems: 'center', gap: `var(${prefixedKey})`}}>
-                    <div style={{ border: '1px solid black', width: '30px', height: '30px' }}></div>
-                    <div style={{ border: '1px solid black', width: '30px', height: '30px' }}></div>
-                  </div>
-                </td>
-                <td>{prefixedKey}</td>
-                <td>{matchingValue}</td>
-              </tr>
-            );
-          default:
-            // For other categories, render an empty div with the computed style
-            return (
-              <tr key={fullKey}>
-                <td>
-                  <div style={style}></div>
-                </td>
-                <td>{prefixedKey}</td>
-                <td>{matchingValue}</td>
-              </tr>
-            );
+              break;
+            case 'border-radius':
+              style.borderRadius = matchingValue;
+              style.width = "100px";
+              style.height = "50px";
+              style.border = "1px solid black";
+              break;
+            case 'box-shadow':
+              style.boxShadow = matchingValue;
+              break;
+            case 'letter-spacing':
+              style.letterSpacing = matchingValue;
+              break;
+            case 'line-height':
+              style.lineHeight = matchingValue;
+              break;
+            case 'font-weight':
+              style.fontWeight = matchingValue;
+              break;
+            case 'font-family':
+              style.fontFamily = matchingValue;
+              break;
+            case 'font-size':
+              style.fontSize = matchingValue;
+              break;
+            case 'spacing':
+              // Render spacing row
+              return (
+                <tr key={fullKey}>
+                  <td>
+                    <div style={{ display: 'flex', alignItems: 'center', gap: `var(${prefixedKey})`}}>
+                      <div style={{ border: '1px solid black', width: '30px', height: '30px' }}></div>
+                      <div style={{ border: '1px solid black', width: '30px', height: '30px' }}></div>
+                    </div>
+                  </td>
+                  <td>{prefixedKey}</td>
+                  <td>{matchingValue}</td>
+                </tr>
+              );
+            default:
+              // For other categories, render an empty div with the computed style
+              return (
+                <tr key={fullKey}>
+                  <td>
+                    <div style={style}></div>
+                  </td>
+                  <td>{prefixedKey}</td>
+                  <td>{matchingValue}</td>
+                </tr>
+              );
+          }
         }
-        console.log('matchingvalue: ', matchingValue);
 
         // Render "Aa" preview only for text-based styles
-      const isTextBasedStyle = ['letter-spacing', 'line-height', 'font-weight', 'font-family', 'font-size'].includes(category);
-      const preview = isTextBasedStyle ? <div style={style}>Aa</div> : <div style={style}></div>;
+        const isTextBasedStyle = ['letter-spacing', 'line-height', 'font-weight', 'font-family', 'font-size'].includes(category);
+        const preview = isTextBasedStyle ? <div style={style}>Aa</div> : <div style={style}></div>;
 
         return (
           <tr key={fullKey}>
@@ -141,12 +140,11 @@ const DocTokenTable: React.FC<DocTokenTableProps> = ({ type, category }) => {
           </tr>
         );
       } else {
-        return renderTableRows(token as Token, fullKey);
+        return <>{renderTableRows(token as Token, fullKey)}</>;
       }
     });
   };
   
-
   return (
     <table>
       <thead>
