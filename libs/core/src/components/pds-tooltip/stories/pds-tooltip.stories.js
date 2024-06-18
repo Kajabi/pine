e6287d5f39b6f215e6467c8e48b631d76c0a8ac0
Quinton Jason <quinton.jason@gmail.com>
2024-06-18 16:23:09 -0500

    fix(tooltip): remove disabled docs and resolve incorrect slot content color

--- libs/core/src/components/pds-tooltip/stories/pds-tooltip.stories.js
+++ libs/core/src/components/pds-tooltip/stories/pds-tooltip.stories.js
@@ -14,12 +14,6 @@ export default {
   title: 'components/Tooltip'
 }
 
-const defaultParameters = {
-  docs: {
-    disable: true
-  }
-};
-
 const BaseTemplate = (args) => html`
   <pds-tooltip content=${args.content} has-arrow=${args.hasArrow} placement=${args.placement}>${args.slot}</pds-tooltip>`;
 
@@ -88,21 +82,17 @@ Default.args = {
   placement: "right",
   slot: "target text"
 };
-Default.parameters = { ...defaultParameters };
 
 export const HTMLContent = HTMLContentTemplate.bind({});
 HTMLContent.args = {
   htmlContent: true,
   placement: "bottom-start",
 };
-HTMLContent.parameters = { ...defaultParameters }
-
 
 export const Positioning = PositionTemplate.bind({});
 Positioning.args = {
   content: "Trigger",
 };
-Positioning.parameters = { ...defaultParameters }
 
 export const NoArrow = BaseTemplate.bind({});
 NoArrow.args = {
@@ -111,4 +101,3 @@ NoArrow.args = {
   placement: "bottom-start",
   slot: "target text"
 };
-NoArrow.parameters = { ...defaultParameters }
