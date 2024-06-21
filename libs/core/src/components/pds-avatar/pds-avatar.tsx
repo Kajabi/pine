ddc2e50ac69a173324a5f5c772216d1c7c82ccae
Phillip Lovelace <phillip.lovelace@kajabi.com>
2024-06-21 10:23:58 -0700

    fix: update old token values in components (#210)
    * fix: correct token values
    
    * fix(button): correct token values
    
    * fix(textarea): correct token values
    
    * test(avatar): update var in tests

--- libs/core/src/components/pds-avatar/pds-avatar.tsx
+++ libs/core/src/components/pds-avatar/pds-avatar.tsx
@@ -114,7 +114,7 @@ export class PdsAvatar {
       // Percentage is average size of icon in relation to total avatar size
       // of all preset sizes found in Figma.
       // Used to allow icons to scale to container size
-      : <pds-icon color="var(--pine-color-primary-400)" icon={userFilled} size="33.53%"></pds-icon>
+      : <pds-icon color="var(--pine-color-blue-400)" icon={userFilled} size="33.53%"></pds-icon>
   );
 
   private classNames = () => (
