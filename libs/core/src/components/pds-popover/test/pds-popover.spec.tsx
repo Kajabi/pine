62429788075a318e65b7d05a2a3e2e644dfbc75c
Quinton Jason <quinton.jason@gmail.com>
2024-11-12 18:52:04 -0600

    test: add tests

--- libs/core/src/components/pds-popover/test/pds-popover.spec.tsx
+++ libs/core/src/components/pds-popover/test/pds-popover.spec.tsx
@@ -36,4 +36,72 @@ describe('pds-popover', () => {
       </pds-popover>
     `);
   });
+
+  it('should open the popover on trigger click', async () => {
+    const page = await newSpecPage({
+      components: [PdsPopover],
+      html: `
+        <pds-popover placement="top">
+          <button>Popover</button>
+        </pds-popover>
+      `,
+    });
+
+    const button = page.root?.querySelector('button');
+    await button?.click();
+    await page.waitForChanges();
+
+    const popover = page.root?.shadowRoot?.querySelector('.pds-popover');
+    expect(popover).toHaveClass('pds-popover--is-open');
+  });
+
+  it('should close the popover on clicking outside', async () => {
+    const page = await newSpecPage({
+      components: [PdsPopover],
+      html: `
+        <pds-popover placement="top">
+          <button>Popover</button>
+        </pds-popover>
+      `,
+    });
+
+    const button = page.root?.querySelector('button');
+    await button?.click();
+    await page.waitForChanges();
+
+    const document = page.doc;
+    await document.body.click();
+    await page.waitForChanges();
+
+    const popover = page.root?.shadowRoot?.querySelector('.pds-popover');
+    expect(popover).not.toHaveClass('pds-popover--is-open');
+  });
+
+  // it('should not close the popover on clicking inside', async () => {
+  //   const page = await newSpecPage({
+  //     components: [PdsPopover],
+  //     html: `
+  //       <pds-popover placement="top">
+  //         <div slot="trigger">
+  //           <pds-button variant="accent">Popover</pds-button>
+  //         </div>
+  //         <div slot="content">
+  //           <div class="popover-content">Content</div>
+  //         </div>
+  //       </pds-popover>
+  //     `,
+  //   });
+
+  //   const button = page.root?.querySelector('.pds-button') as HTMLElement;
+  //   await button?.click();
+  //   await page.waitForChanges();
+
+  //   const content = page.root?.shadowRoot?.querySelector('.popover-content') as HTMLElement;
+  //   await content?.click();
+  //   await page.waitForChanges();
+
+  //   const popover = page.root?.shadowRoot?.querySelector('.pds-popover');
+  //   console.log('popover', popover);
+  //   expect(popover).toHaveClass('pds-popover--is-open');
+  // });
 });
