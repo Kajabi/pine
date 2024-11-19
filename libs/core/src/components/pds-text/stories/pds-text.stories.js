43d2a034951985dd7466710bd552a0615beaba73
Ashley Echols <ashley.echols@kajabi.com>
2024-11-19 12:55:55 -0600

    feat: adds styles for custom truncate attribute

--- libs/core/src/components/pds-text/stories/pds-text.stories.js
+++ libs/core/src/components/pds-text/stories/pds-text.stories.js
@@ -11,8 +11,8 @@ export default {
 const BaseTemplate = (args) => html`<pds-text
   align="${args.align}"
   color="${args.color}"
-  tag="${args.tag}"
   size="${args.size}"
+  tag="${args.tag}"
   weight="${args.weight}"
 >
   ${args.slot}
@@ -22,4 +22,21 @@ export const Default = BaseTemplate.bind();
 Default.args = {
   slot: 'Hello World',
   tag: 'h1',
+};
+
+const TruncateTemplate = (args) => html`<pds-text
+  align="${args.align}"
+  color="${args.color}"
+  size="${args.size}"
+  tag="${args.tag}"
+  weight="${args.weight}"
+  truncate
+>
+  ${args.slot}
+</pds-text>`;
+
+export const Truncate = TruncateTemplate.bind();
+Truncate.args = {
+  slot: 'loremId irure id magna ipsum voluptate irure esse eu nulla. Ullamco officia adipisicing qui nulla non sint. Mollit tempor veniam quis nisi aliqua duis elit eu laborum et incididunt ut sit irure. Nisi aute veniam sint do amet consectetur velit. Quis sunt enim mollit deserunt laboris dolor elit exercitation. Id labore deserunt sint consequat laboris nulla do ut magna. Aliquip labore esse sint consequat voluptate tempor consectetur sit sint culpa occaecat ut velit est.',
+  tag: 'p',
 };
