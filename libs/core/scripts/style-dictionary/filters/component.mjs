78a423f02012fcc370a780dedeca0653f9ae3321
Julian Skinner <ju.skinner@gmail.com>
2024-11-19 10:40:46 -0600

    style(token-studio): refactor component generator

--- libs/core/scripts/style-dictionary/filters/component.mjs
+++ libs/core/scripts/style-dictionary/filters/component.mjs
@@ -1,11 +1,9 @@
+import fs from 'fs-extra';
+
+const basePath = `src/global/styles/tokens`;
+
 // filters tokens by themable and from which tokenset they originate
 // must match per component name, in this repository we currently only have "button"
 export const componentFilter =
-  (cat, themeable = false) =>
-    (token) => {
-      const tokenThemable = Boolean(token.attributes.themeable);
-
-      return (
-        themeable === tokenThemable && token.filePath.endsWith(`${cat}.json`)
-      );
-  };
+  (cat) =>
+    (token) => token.filePath.startsWith(`${basePath}/components/${cat}.json`);
