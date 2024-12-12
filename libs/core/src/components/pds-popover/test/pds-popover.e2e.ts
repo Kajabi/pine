90bd5d79ba1b9a5e91884929656dbd2ded139f4f
Quinton Jason <quinton.jason@gmail.com>
2024-12-12 12:42:24 -0600

    test: update e2e tests

--- libs/core/src/components/pds-popover/test/pds-popover.e2e.ts
+++ libs/core/src/components/pds-popover/test/pds-popover.e2e.ts
@@ -9,76 +9,55 @@ describe('pds-popover', () => {
     expect(element).toHaveClass('hydrated');
   });
 
-  it('should show the popover on trigger click', async () => {
+  it('should handle trigger click interactions', async () => {
     const page = await newE2EPage();
     await page.setContent('<pds-popover component-id="my-popover" text="Show popover"></pds-popover>');
-
-    const triggerButton = await page.find('pds-popover >>> .pds-popover__trigger') as unknown as HTMLButtonElement;
-    expect(triggerButton).toBeTruthy();
-
-    const popoverContent = await page.find('pds-popover >>> div[popover]') as unknown as HTMLElement;
-    expect(popoverContent).toBeTruthy();
-    expect(popoverContent.classList.contains('pds-popover--active')).toBeFalsy();
-
-    await triggerButton?.click();
-    await page.waitForChanges();
-
-    expect(popoverContent.classList.contains('pds-popover--active')).toBeTruthy();
-  });
-
-  it('should handle keyboard interactions', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<pds-popover component-id="my-popover" text="Show popover"></pds-popover>');
-
-    const triggerButton = await page.find('pds-popover >>> .pds-popover__trigger');
-    const popoverContent = await page.find('pds-popover >>> div[popover]');
-
-    // Test Enter key
-    await triggerButton.press('Enter');
-    await page.waitForChanges();
-    expect(await popoverContent.getAttribute('class')).toContain('pds-popover--active');
-
-    // Test Escape key
-    await page.keyboard.press('Escape');
-    await page.waitForChanges();
-    expect(await popoverContent.getAttribute('class')).not.toContain('pds-popover--active');
-
-    // Test Space key
-    await triggerButton.press(' ');
-    await page.waitForChanges();
-    expect(await popoverContent.getAttribute('class')).toContain('pds-popover--active');
-  });
-
-  it('should respect manual mode', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<pds-popover component-id="my-popover" text="Show popover" popover-type="manual"></pds-popover>');
 
     const triggerButton = await page.find('pds-popover >>> .pds-popover__trigger');
     const popoverContent = await page.find('pds-popover >>> div[popover]');
 
     await triggerButton.click();
     await page.waitForChanges();
-    expect(await popoverContent.getAttribute('class')).toContain('pds-popover--active');
+    expect(await popoverContent.getProperty('className')).toContain('pds-popover--active');
 
-    // Click outside should not close in manual mode
-    await page.click('body');
+    await triggerButton.click();
     await page.waitForChanges();
-    expect(await popoverContent.getAttribute('class')).toContain('pds-popover--active');
+    expect(await popoverContent.getProperty('className')).not.toContain('pds-popover--active');
   });
 
-  it('should handle show/hide methods', async () => {
+  it('should update position on scroll when active', async () => {
     const page = await newE2EPage();
-    await page.setContent('<pds-popover component-id="my-popover" text="Show popover"></pds-popover>');
+    await page.setContent('<div style="height: 200vh"><pds-popover component-id="my-popover" text="Show popover"></pds-popover></div>');
 
-    const element = await page.find('pds-popover');
+    const triggerButton = await page.find('pds-popover >>> .pds-popover__trigger');
     const popoverContent = await page.find('pds-popover >>> div[popover]');
 
-    await element.callMethod('show');
+    await triggerButton.click();
     await page.waitForChanges();
-    expect(await popoverContent.getAttribute('class')).toContain('pds-popover--active');
 
-    await element.callMethod('hide');
+    const initialTop = await popoverContent.getComputedStyle('top');
+
+    await page.evaluate(() => window.scrollBy(0, 100));
     await page.waitForChanges();
-    expect(await popoverContent.getAttribute('class')).not.toContain('pds-popover--active');
+
+    const newTop = await popoverContent.getComputedStyle('top');
+    expect(newTop).not.toBe(initialTop);
+  });
+
+  it('should respect manual mode', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<pds-popover component-id="my-popover" popover-type="manual" text="Show popover"></pds-popover>');
+
+    const triggerButton = await page.find('pds-popover >>> .pds-popover__trigger');
+    const popoverContent = await page.find('pds-popover >>> div[popover]');
+
+    await triggerButton.click();
+    await page.waitForChanges();
+    expect(await popoverContent.getProperty('className')).toContain('pds-popover--active');
+
+    // Click outside should not close in manual mode
+    await page.mouse.click(0, 0);
+    await page.waitForChanges();
+    expect(await popoverContent.getProperty('className')).toContain('pds-popover--active');
   });
 });
