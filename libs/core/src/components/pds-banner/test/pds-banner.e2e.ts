d96ed421c596f9396ebef06fe3081a2f7b6d619a
Ashley Echols <ashley.echols@kajabi.com>
2025-05-20 14:45:14 -0500

    test(banner): adds tests

--- libs/core/src/components/pds-banner/test/pds-banner.e2e.ts
+++ libs/core/src/components/pds-banner/test/pds-banner.e2e.ts
@@ -8,4 +8,72 @@ describe('pds-banner', () => {
     const element = await page.find('pds-banner');
     expect(element).toHaveClass('hydrated');
   });
+
+  it('renders a dismissable banner when the dismissable prop is set', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<pds-banner dismissable></pds-banner>');
+
+    const closeButton = await page.find('pds-banner >>> .pds-banner__close');
+    expect(closeButton).toBeTruthy();
+  });
+
+  it('dismisses a banner when the close button is clicked', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<pds-banner active dismissable></pds-banner>');
+
+    const banner = await page.find('pds-banner');
+
+    expect(banner).toHaveClass('pds-banner--active');
+
+    const closeButton = await page.find('pds-banner >>> .pds-banner__close');
+    await closeButton.click();
+
+    await page.waitForChanges();
+
+    expect(banner).not.toHaveClass('pds-banner--active');
+  });
+
+  it('emits a pdsDismiss event when the close button is clicked', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<pds-banner active dismissable></pds-banner>');
+
+    const eventSpy = await page.spyOnEvent('pdsDismiss');
+
+    const closeButton = await page.find('pds-banner >>> .pds-banner__close');
+    await closeButton.click();
+
+    expect(eventSpy).toHaveReceivedEvent();
+  });
+
+  it('emits a pdsBannerActivated event when the banner is activated', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<pds-banner></pds-banner>');
+
+    const eventSpy = await page.spyOnEvent('pdsBannerActivated');
+
+    const banner = await page.find('pds-banner');
+    banner.setAttribute('active', true);
+
+    await page.waitForChanges();
+
+    expect(eventSpy).toHaveReceivedEvent();
+  });
+
+  it('dismisses a banner when another banner is activated', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<pds-banner component-id="banner-1" active></pds-banner><pds-banner component-id="banner-2"></pds-banner>');
+
+    const eventSpy = await page.spyOnEvent('pdsBannerActivated');
+
+    const banner1 = await page.find('#banner-1');
+    expect(banner1).toHaveClass('pds-banner--active');
+    const banner2 = await page.find('#banner-2');
+    banner2.setAttribute('active', true);
+
+    await page.waitForChanges();
+
+    expect(eventSpy).toHaveReceivedEvent();
+    expect(banner1).not.toHaveClass('pds-banner--active');
+    expect(banner2).toHaveClass('pds-banner--active');
+  });
 });
--- libs/core/src/components/pds-banner/test/pds-banner.spec.tsx
+++ libs/core/src/components/pds-banner/test/pds-banner.spec.tsx
@@ -8,9 +8,97 @@ describe('pds-banner', () => {
       html: `<pds-banner></pds-banner>`,
     });
     expect(page.root).toEqualHtml(`
-      <pds-banner>
+      <pds-banner class="pds-banner" variant="default">
         <mock:shadow-root>
-          <slot></slot>
+          <pds-box
+            background-color="var(--banner-background-color)"
+            fit
+            padding-block-start="sm"
+            padding-block-end="sm"
+            padding-inline-start="md"
+            padding-inline-end="md"
+          >
+            <pds-box display="flex" justify-content="space-between">
+              <pds-box display="flex" align-items="center" gap="xs">
+                <pds-icon color="var(--banner-icon-color)" name="info-circle-filled"></pds-icon>
+                <pds-text color="var(--banner-text-color)" tag="p">
+                  <slot name="text"></slot>
+                </pds-text>
+              </pds-box>
+              <pds-box display="flex" align-items="center" gap="sm" justify-content="end">
+                <slot name="actions"></slot>
+              </pds-box>
+            </pds-box>
+          </pds-box>
+        </mock:shadow-root>
+      </pds-banner>
+    `);
+  });
+
+  it('renders a variant when the variant prop is set', async () => {
+    const page = await newSpecPage({
+      components: [PdsBanner],
+      html: `<pds-banner variant="warning"></pds-banner>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <pds-banner class="pds-banner" variant="warning">
+        <mock:shadow-root>
+          <pds-box
+            background-color="var(--banner-background-color)"
+            fit
+            padding-block-start="sm"
+            padding-block-end="sm"
+            padding-inline-start="md"
+            padding-inline-end="md"
+          >
+            <pds-box display="flex" justify-content="space-between">
+              <pds-box display="flex" align-items="center" gap="xs">
+                <pds-icon color="var(--banner-icon-color)" name="info-circle-filled"></pds-icon>
+                <pds-text color="var(--banner-text-color)" tag="p">
+                  <slot name="text"></slot>
+                </pds-text>
+              </pds-box>
+              <pds-box display="flex" align-items="center" gap="sm" justify-content="end">
+                <slot name="actions"></slot>
+              </pds-box>
+            </pds-box>
+          </pds-box>
+        </mock:shadow-root>
+      </pds-banner>
+    `);
+  });
+
+  it('renders a dismissable banner when the dismissable prop is set', async () => {
+    const page = await newSpecPage({
+      components: [PdsBanner],
+      html: `<pds-banner dismissable></pds-banner>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <pds-banner class="pds-banner" variant="default" dismissable>
+        <mock:shadow-root>
+          <pds-box
+            background-color="var(--banner-background-color)"
+            fit
+            padding-block-start="sm"
+            padding-block-end="sm"
+            padding-inline-start="md"
+            padding-inline-end="md"
+          >
+            <pds-box display="flex" justify-content="space-between">
+              <pds-box display="flex" align-items="center" gap="xs">
+                <pds-icon color="var(--banner-icon-color)" name="info-circle-filled"></pds-icon>
+                <pds-text color="var(--banner-text-color)" tag="p">
+                  <slot name="text"></slot>
+                </pds-text>
+              </pds-box>
+              <pds-box display="flex" align-items="center" gap="sm" justify-content="end">
+                <slot name="actions"></slot>
+                <button class="pds-banner__close" aria-label="Dismiss banner">
+                  <pds-icon color="var(--banner-icon-color)" name="remove" aria-hidden="true"></pds-icon>
+                </button>
+              </pds-box>
+            </pds-box>
+          </pds-box>
         </mock:shadow-root>
       </pds-banner>
     `);
