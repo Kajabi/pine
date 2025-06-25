8f512b1dadc330cf8456f92be9bd90bddd94ff12
Quinton Jason <quinton.jason@gmail.com>
2025-06-25 12:14:58 -0500

    fix: update figma mappings

--- libs/core/src/components/pds-input/pds-input.figma.ts
+++ libs/core/src/components/pds-input/pds-input.figma.ts
@@ -1,34 +1,38 @@
 import figma, { html } from '@figma/code-connect/html';
 
+const sharedProps = {
+  disabled: figma.enum("State", {
+    disabled: "true",
+  }),
+  errorMessage: figma.nestedProps(".error-message", {
+    text: figma.string("Error content"),
+  }),
+  helperMessage: figma.boolean("Has help message", {
+    true: figma.string("Help message"),
+    false: undefined,
+  }),
+  invalid: figma.enum("State", {
+    error: "true",
+  }),
+  label: figma.boolean("Has label", {
+    true: figma.string("Label"),
+    false: undefined,
+  }),
+  prefix: figma.boolean("Prefix", {
+    true: "Prefix",
+    false: undefined,
+  }),
+  readonly: figma.enum("State", {
+    'read-only': "true",
+  }),
+  value: figma.nestedProps(".Input text", {
+    text: figma.string("Message"),
+  }),
+}
+
 figma.connect('https://www.figma.com/design/CC1YmaGKHnsvB28yLY9mEH?node-id=34603-6095', {
   props: {
-    disabled: figma.enum("State", {
-      disabled: "true",
-    }),
-    errorMessage: figma.nestedProps(".error-message", {
-      text: figma.string("Error content"),
-    }),
-    helperMessage: figma.boolean("Has help message", {
-      true: figma.string("Help message"),
-      false: undefined,
-    }),
-    invalid: figma.enum("State", {
-      error: "true",
-    }),
-    label: figma.boolean("Has label", {
-      true: figma.string("Label"),
-      false: undefined,
-    }),
-    prefix: figma.boolean("Prefix", {
-      true: "Prefix",
-      false: undefined,
-    }),
-    readonly: figma.enum("State", {
-      'read-only': "true",
-    }),
-    value: figma.nestedProps(".Input text", {
-      text: figma.string("Message"),
-    }),
+    ...sharedProps,
   },
   example: (props) => html`\
     <pds-input
@@ -38,6 +42,24 @@ figma.connect('https://www.figma.com/design/CC1YmaGKHnsvB28yLY9mEH?node-id=34603
       invalid=${props.invalid}
       label=${props.label}
       readonly=${props.readonly}
+      value=${props.value.text}
+    ></pds-input>
+  `,
+});
+
+figma.connect('https://www.figma.com/design/CC1YmaGKHnsvB28yLY9mEH?node-id=35874-14473', {
+  props: {
+    ...sharedProps,
+  },
+  example: (props) => html`\
+    <pds-input
+      disabled=${props.disabled}
+      error-message=${props.errorMessage.text}
+      helper-message=${props.helperMessage}
+      invalid=${props.invalid}
+      label=${props.label}
+      readonly=${props.readonly}
+      type="number"
       value=${props.value.text}
     ></pds-input>
   `,
