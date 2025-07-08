fd80586831ec31b6784d82b7ec28c13895085c9f
Quinton Jason <quinton.jason@gmail.com>
2025-07-08 10:48:15 -0500

    fix: add tabs and toast

--- libs/core/src/components/pds-tabs/pds-tabs.figma.ts
+++ libs/core/src/components/pds-tabs/pds-tabs.figma.ts
@@ -1,5 +1,6 @@
 import figma, { html } from '@figma/code-connect/html';
 
+// Tab
 figma.connect('https://www.figma.com/design/CC1YmaGKHnsvB28yLY9mEH?node-id=37112-19825', {
   props: {
     variant: figma.enum('Variant', {
@@ -7,13 +8,14 @@ figma.connect('https://www.figma.com/design/CC1YmaGKHnsvB28yLY9mEH?node-id=37112
       "filter": "pill",
       "availability": "availability",
     }),
-    label: figma.textContent('Text Layer'),
+    label: figma.textContent('Label'),
   },
   example: (props) => html`\
   <pds-tab variant=${props.variant}>${props.label}</pds-tab>
   `,
 });
 
+// Tabs
 figma.connect('https://www.figma.com/design/CC1YmaGKHnsvB28yLY9mEH?node-id=2537-21031', {
   props: {
     variant: figma.enum('Variant', {
