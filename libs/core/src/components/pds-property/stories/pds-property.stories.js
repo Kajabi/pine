f979b608a769549ef6354def93cf27b1287fc1e1
Phillip Lovelace <phillip.lovelace@kajabi.com>
2025-07-14 13:43:41 -0700

    docs(pds-property): enhance documentation with default icon behavior and usage guidelines

--- libs/core/src/components/pds-property/stories/pds-property.stories.js
+++ libs/core/src/components/pds-property/stories/pds-property.stories.js
@@ -4,36 +4,17 @@ import { withActions } from '@storybook/addon-actions/decorator';
 
 export default {
   argTypes: extractArgTypes('pds-property'),
-  args: {
-    icon: null,
-    text: null,
-  },
   component: 'pds-property',
   decorators: [withActions],
   title: 'components/Property',
 };
 
 const BaseTemplate = (args) => html`
-  <pds-property
-    component-id=${args.componentId}
-    icon=${args.icon}
-  >
-    ${args.text}
-  </pds-property>`;
+  <pds-property component-id=${args.componentId} icon=${args.icon}>${args.text}</pds-property>`;
 
 export const Default = BaseTemplate.bind();
 Default.args = {
-  icon: 'check-circle',
+  componentId: 'property-1',
+  icon: 'star',
   text: 'Property text',
-};
-
-export const WithoutIcon = BaseTemplate.bind();
-WithoutIcon.args = {
-  text: 'Property text without icon',
-};
-
-export const LongText = BaseTemplate.bind();
-LongText.args = {
-  icon: 'warning-triangle',
-  text: 'This is a longer property text that might wrap to multiple lines or be truncated depending on the container width',
 };
