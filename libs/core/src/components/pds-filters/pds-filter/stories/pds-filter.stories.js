83c0957aee2987b302c2d96f49a5712994b4b2e7
Phillip Lovelace <phillip.lovelace@kajabi.com>
2025-09-26 13:00:05 -0700

    feat(pds-filter): add documentation and stories for pds-filter component

---
+++ libs/core/src/components/pds-filters/pds-filter/stories/pds-filter.stories.js
@@ -0,0 +1,93 @@
+import { html } from 'lit';
+import { extractArgTypes } from '@pxtrn/storybook-addon-docs-stencil';
+import { withActions } from '@storybook/addon-actions/decorator';
+
+export default {
+  argTypes: {
+    ...extractArgTypes('pds-filter'),
+    variant: {
+      control: {
+        type: 'select',
+      },
+      options: ['default', 'selected', 'more', 'clear'],
+    },
+  },
+  component: 'pds-filter',
+  decorators: [withActions],
+  parameters: {
+    actions: {
+      handles: ['pdsFilterOpen', 'pdsFilterClose', 'pdsFilterClear'],
+    },
+  },
+  title: 'components/Filters/Filter',
+};
+
+const BaseTemplate = (args) => html`
+  <pds-filter
+    component-id="${args.componentId}"
+    variant="${args.variant}"
+    text="${args.text}"
+    icon="${args.icon}"
+  >
+    <p>Filter options go here</p>
+  </pds-filter>
+`;
+
+export const Default = BaseTemplate.bind();
+Default.args = {
+  componentId: 'filter-default',
+  variant: 'default',
+  text: 'Trigger',
+  icon: 'flash',
+};
+
+export const Selected = BaseTemplate.bind();
+Selected.args = {
+  componentId: 'filter-selected',
+  variant: 'selected',
+  text: 'Grant an offer',
+  icon: 'switch-vertical',
+};
+
+export const More = BaseTemplate.bind();
+More.args = {
+  componentId: 'filter-more',
+  variant: 'more',
+  text: 'More Filters',
+  icon: 'add-circle',
+};
+
+export const Clear = (args) => html`
+  <pds-filter
+    component-id="${args.componentId}"
+    variant="clear"
+    text="${args.text}"
+  >
+  </pds-filter>
+`;
+Clear.args = {
+  componentId: 'filter-clear',
+  text: 'Clear all',
+};
+
+const VariantsTemplate = () => html`
+  <pds-filters component-id="all-variants">
+    <pds-filter component-id="variant-default" variant="default" text="Trigger" icon="flash">
+      <p>Filter options go here</p>
+    </pds-filter>
+
+    <pds-filter component-id="variant-selected" variant="selected" text="Grant an offer" icon="switch-vertical">
+      <p>Filter options go here</p>
+    </pds-filter>
+
+    <pds-filter component-id="variant-more" variant="more" text="More filters" icon="add-circle">
+      <p>Filter options go here</p>
+    </pds-filter>
+
+    <pds-filter component-id="variant-clear" variant="clear" text="Clear all">
+    </pds-filter>
+  </pds-filters>
+`;
+
+export const AllVariants = VariantsTemplate.bind();
+AllVariants.args = {};
--- libs/core/src/components/pds-filters/stories/pds-filters.stories.js
+++ libs/core/src/components/pds-filters/stories/pds-filters.stories.js
@@ -17,16 +17,16 @@ export default {
 const BaseTemplate = (args) => html`
   <pds-filters component-id="${args.componentId}">
     <pds-filter component-id="filter-1" variant="default" text="Trigger" icon="flash">
-      <p>Trigger filter options</p>
+      <p>Filter options go here</p>
     </pds-filter>
     <pds-filter component-id="filter-2" variant="selected" text="Grant an offer" icon="switch-vertical">
-      <p>Selected action filter</p>
+      <p>Filter options go here</p>
     </pds-filter>
     <pds-filter component-id="filter-3" variant="default" text="Status" icon="activity">
-      <p>Status filter options</p>
+      <p>Filter options go here</p>
     </pds-filter>
     <pds-filter component-id="filter-4" variant="more" text="More filters" icon="add-circle">
-      <p>Additional filter options</p>
+      <p>Filter options go here</p>
     </pds-filter>
     <pds-filter component-id="filter-5" variant="clear" text="Clear all">
     </pds-filter>
