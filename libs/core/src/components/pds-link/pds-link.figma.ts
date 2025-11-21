bb6d4eab513ab0277b2598ae63a744d555ba462b
Phillip Lovelace <phillip.lovelace@kajabi.com>
2025-11-20 16:12:12 -0800

    fix(pds-divider, pds-link, pds-progress): update code connect properties and examples

--- libs/core/src/components/pds-link/pds-link.figma.ts
+++ libs/core/src/components/pds-link/pds-link.figma.ts
@@ -3,27 +3,30 @@ import figma, { html } from '@figma/code-connect/html';
 figma.connect('<FIGMA_LINK>', {
   props: {
     color: figma.enum('Variant', {
-      'destructive': 'destructive',
+      'destructive': 'danger',
       'accent': 'accent',
+      'inverted': 'secondary',
     }),
     external: figma.enum('Variant', {
       'external': true,
     }),
     fontSize: figma.enum('Size', {
       'md': 'md',
-      'lg': 'lg',
+      'sm': 'sm',
     }),
     label: figma.string('Label'),
     variant: figma.enum('Variant', {
       'plain': 'plain',
+      'inline': 'inline',
     }),
   },
-  example: (props) => html`
+  example: (props) => html`\
   <pds-link
     color=${props.color}
     external=${props.external}
     font-size=${props.fontSize}
     variant=${props.variant}
-    >${props.label}
+  >
+    ${props.label}
   </pds-link>`,
 });
--- libs/core/src/components/pds-progress/pds-progress.figma.ts
+++ libs/core/src/components/pds-progress/pds-progress.figma.ts
@@ -1,27 +1,66 @@
 import figma, { html } from '@figma/code-connect/html';
 
 figma.connect('<FIGMA_PROGRESS_BAR>', {
-    props: {
-      percent: figma.string("Progress"),
-      showPercent: figma.enum("Label", {
-        "false": true,
-      }),
-    },
-    example: (props) => html`\
-      <pds-progress
-        percent=${props.percent}
-        show-percent=${props.showPercent}
-      ></pds-progress>
-    `,
-  });
+  props: {
+    percent: figma.string("Progress"),
+    showPercent: figma.enum("Label", {
+      "Right": true,
+    }),
+  },
+  example: (props) => html`\
+<pds-progress
+  percent=${props.percent}
+  show-percent=${props.showPercent}
+></pds-progress>
+  `,
+  variant: { "Label": "False" },
+});
+
+figma.connect('<FIGMA_PROGRESS_BAR>', {
+  props: {
+    percent: figma.string("Progress"),
+    showPercent: figma.enum("Label", {
+      "Right": true,
+    }),
+  },
+  example: (props) => html`\
+<pds-progress
+  percent=${props.percent}
+  show-percent=${props.showPercent}
+></pds-progress>
+  `,
+  variant: { "Label": "Right" },
+});
+
+figma.connect('<FIGMA_PROGRESS_BAR>', {
+  props: {
+    percent: figma.string("Progress"),
+  },
+  example: (props) => html`\
+<pds-tooltip content=${props.percent} has-arrow="false" placement="top" style="width: 100%">
+  <pds-progress percent=${props.percent}></pds-progress>
+</pds-tooltip>
+  `,
+  variant: { "Label": "Top floating" },
+});
+
+figma.connect('<FIGMA_PROGRESS_BAR>', {
+  props: {
+    percent: figma.string("Progress"),
+  },
+  example: (props) => html`\
+<pds-tooltip content=${props.percent} has-arrow="false" placement="bottom" style="width: 100%">
+  <pds-progress percent=${props.percent}></pds-progress>
+</pds-tooltip>
+  `,
+  variant: { "Label": "Bottom floating" },
+});
 
 figma.connect('<FIGMA_PROGRESS_CIRCLE>', {
   props: {
     percent: figma.string("Completion"),
   },
   example: (props) => html`\
-    <pds-progress
-      percent=${props.percent}
-    ></pds-progress>
+<pds-progress percent=${props.percent}></pds-progress>
   `,
 });
