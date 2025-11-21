78c41cf13054613fcf5d7c3be560415ecfa60df7
Phillip Lovelace <phillip.lovelace@kajabi.com>
2025-11-21 09:18:35 -0800

    fix(pds-divider, pds-link, pds-progress): update code connect properties and examples (#595)
    * fix(pds-divider, pds-link, pds-progress): update code connect properties and examples
    
    * fix(pds-link): update fontSize property
    
    * fix(pds-link): add default href attribute to link component

--- libs/core/src/components/pds-divider/pds-divider.figma.ts
+++ libs/core/src/components/pds-divider/pds-divider.figma.ts
@@ -2,10 +2,14 @@ import figma, { html } from '@figma/code-connect/html';
 
 figma.connect('<FIGMA_DIVIDER>', {
   props: {
-    offset: figma.enum('Bleed', {
-      "16px bleed": "sm",
-      "24px bleed": "md",
+    offset: figma.enum('Offset', {
+      "xxs - 4px": "xxs",
+      "xs - 8px bleed": "xs",
+      "sm - 16px bleed": "sm",
+      "md - 24px bleed": "md",
+      "lg - 32px bleed": "lg",
+      "xl - 48px": "xl",
     }),
   },
-  example: (props) => html`<pds-divider bleed=${props.offset}></pds-divider>`,
+  example: (props) => html`<pds-divider offset=${props.offset}></pds-divider>`,
 });
