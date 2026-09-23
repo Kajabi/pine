ac3217df6c6f0969506f785fda2ff276e84e104f
Phillip Lovelace <phillip.lovelace@kajabi.com>
2026-09-23 09:44:17 -0700

    docs(pds-box): remove redundant per-side borders story

--- libs/core/src/components/pds-box/stories/pds-box.stories.js
+++ libs/core/src/components/pds-box/stories/pds-box.stories.js
@@ -62,22 +62,3 @@ Default.args = {
   border: true,
   minHeight: '100px',
 };
-
-export const PerSideBorders = () => html`
-<pds-box direction="column" gap="lg">
-  <pds-box direction="column">
-    <pds-box border-block-end="true" padding="sm">Divider row one</pds-box>
-    <pds-box border-block-end="true" padding="sm">Divider row two</pds-box>
-    <pds-box padding="sm">Last row, no divider</pds-box>
-  </pds-box>
-
-  <pds-box>
-    <pds-box border-inline-end="true" padding="sm" min-width="140px">Sidebar rail</pds-box>
-    <pds-box padding="sm">Main content</pds-box>
-  </pds-box>
-
-  <pds-box direction="column">
-    <pds-box border="true" padding="sm">All four sides</pds-box>
-    <pds-box border="true" border-block-start="false" padding="sm">Block-start removed</pds-box>
-  </pds-box>
-</pds-box>`;
